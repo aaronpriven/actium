47e5d0ff921ce49cdc3701d833c6be3b679581fb
Aaron Priven <aaron@priven.com>
2000-06-29 13:58:24 -0700

    Adding newsignup.pl (2002-08/acsched.pl)

---
+++ bin/newsignup.pl
@@ -0,0 +1,461 @@
+#!/usr/bin/perl5
+
+# Note that this assumes that the files are in the native text form
+# (for DOS/Windows/NT, that is, the line ends are CR/LF). 
+
+chdir &get_directory or die "Can't change to specified directory.\n";
+
+# so we cd to the appropriate directory
+# The &get_directory routine is currently OS-specific, or at least,
+# depends on the presence of a command line.
+
+&assemble_line_and_file_lists;
+
+# now @lines is a list of lines, and @scdfiles is a list of scdfiles
+
+foreach $linenum (@lines) {
+
+   print "$linenum ";
+
+   undef %fullsched;
+   # reset the full schedule variable before the loop.
+
+   foreach $schedfile (&get_scheds_for_line ($linenum)) {
+      
+      undef @toplines;
+      undef @schedrows;
+      # reset the various other arrays before the loop.
+
+      open IN, "<$schedfile" or die "Can't open file $schedfile.\n"; 
+
+      $schedname = $schedfile;
+      $schedname =~ s/^AC_${linenum}_//;
+      $schedname =~ s/.scd$//;
+
+#      print $schedname , " ";
+
+      &get_schedule_info;
+
+      # now @toplines is set to the first two lines,
+      # %fullsched{$schedname}{NOTEDEFS} is set to the note definitions,
+      # and @schedrows has all the rest of the schedule lines.
+
+      &get_timepoint_info;
+
+      close (IN);
+      # close the schedule
+
+      &parse_schedule;
+
+   }   
+
+
+   &merge_days ("SA" , "SU" , "WE");
+   &merge_days ("WD" , "WE" , "DA");
+
+   # if we are likely to have any other possible mergers,
+   # -- e.g. weekday and Saturday schedules being the same, but different
+   # from Sunday -- we can add those. But I think that's unlikely.
+
+   &output_schedule;
+
+}
+
+print "\n\n";
+
+sub get_scheds_for_line {
+
+  my $line = $_[0];
+
+  return grep ( /^AC_${line}_/ , @scdfiles);
+
+}
+
+### GET_DIRECTORY
+
+sub get_directory {
+
+die "No directory given in command line.\n" unless (@ARGV);
+
+return $ARGV[0];
+
+}
+
+
+### ASSEMBLE_LINE_AND_FILE_LISTS
+
+sub assemble_line_and_file_lists {
+
+@scdfiles = sort <*.scd>;
+
+# so @scdfiles has all the files in it, sorted
+
+my %lines = ();
+
+foreach $dummyvar (@scdfiles) {
+
+   $_ = $dummyvar;
+
+   # one has to use the $dummyvar and then the assignment, 
+   # otherwise the next two statements modify the @scdfiles array,
+   # which is not what we want.
+ 
+   s/^AC_//;
+   s/_.*//;
+
+   next if $_ eq "56";
+
+   # THIS IS SPECIAL. LINE 56 IS BROKEN BECAUSE THERE ARE TWO SEPARATE 
+   # SCHEDULES FOR LINE 56
+   # (THE LATE NIGHT SCHEDULE AND THE REGULAR SCHEDULE). IF THIS 
+   # CHANGES, THE ABOVE CODE SHOULD BE REMOVED.
+   # In the meantime, we'll have to do line 56 by hand.
+
+   $lines{"$_"} = 1;
+
+}
+
+# So now keys %lines contains all the lines.
+
+#@lines = sort { $a <=> $b or $a cmp $b} keys %lines;
+
+# that wonderful sort says "sort numerically, but if they're the same
+#  numerically, use alphabetical sort."
+# Unfortunately, it ends up with the numbers last instead of first.
+#  sigh.
+
+@lines = sort byroutes keys %lines;
+
+}
+
+
+sub byroutes  {
+
+   my ($aa, $bb, $anum, $bnum);
+
+   $aa = uc($a);
+   $bb = uc($b);
+   
+   $anum = $aa > 0;
+   $bnum = $bb > 0;
+   # So, $anum is true if $a is a number, etc.
+
+   return ($bnum <=> $anum) unless $anum == $bnum;
+
+   #  If they're not the same, return whichever one is lesser
+   #  (which will be whichever one is the number).
+
+   return ($aa <=> $bb) if ($anum and $bnum);
+ 
+   # if $a and $b are both numbers, return a numeric comparison
+
+   return ($aa cmp $bb);
+
+   # otherwise they are both strings, so return the string comparison
+
+}
+
+
+sub get_schedule_info {
+
+   ### Get the schedule info ####
+   
+   $_ = <IN>; 
+   # blank line at the top of the file is thrown away
+
+   $toplines[0] = <IN> ;
+   $toplines[1] = <IN> ;
+   chomp (@toplines);
+   # toplines gets the first two lines
+
+   $_ = <IN>;
+
+   until (m@timepoints@) {
+
+      # keep reading data until the "key to timepoints" is reached
+
+      chomp;
+
+      next unless $_;
+      next if /RTE/;
+      next if /NUM/;
+
+      # go to the next one if it's blank, or if it has RTE or NUM in it,
+      # which means it must be a line with repeated timepoint info, which
+      # we do not need.
+
+      if (substr($_, 3, 1) eq "-") {
+
+          $note = substr($_,0, 2);
+
+          $note =~ s/\s+//g;
+          # strip spaces from $note
+
+          $notedef = substr($_, 5);
+
+          push @{$fullsched{$schedname}{"NOTEDEFS"}}, "$note:$notedef";
+          next;
+
+      }
+
+      # If it's got a hyphen in the fourth position, that means we think
+      # it's a note definition, and so we add it to {"NOTEDEFS"}
+ 
+      push @schedrows, $_;
+ 
+      # otherwise, we add it to the array.
+
+      } continue {
+
+         # this bit is what happens after the "next" in the lines above.
+
+         $_ = <IN>;
+         # load another line.
+
+      }
+
+   # @schedrows now has all the raw schedule information in it.
+   # @toplines now has the raw timepoint information in it.
+   # %fullsched{$schedname}{"NOTEDEFS"} now has all the note definitions.
+
+}
+
+sub get_timepoint_info {
+
+   while (<IN>) {
+   # keep reading data until the end of the file
+   
+      chomp;
+      $_ = substr ($_, 11);
+      push @{$fullsched{$schedname}{"TIMEPOINTS"}}, $_ if $_;
+
+   } 
+
+   # {TIMEPOINTS} is now the list of timepoints
+
+}
+
+sub parse_schedule {
+
+### get notes and routes
+
+   foreach (@schedrows) {
+       push @{$fullsched{$schedname}{"SPECDAYS"}}, substr($_,0,2);
+       push @{$fullsched{$schedname}{"NOTES"}}, substr($_,2,1);
+       push @{$fullsched{$schedname}{"ROUTES"}}, substr($_,3,5);
+       $_ = substr($_,8);
+   }
+   # that takes the notes and routes out of @schedrows and puts it 
+   # the appropriate part of %fullsched instead.
+
+   foreach (@toplines) {
+       $_ = substr($_,8)
+   }
+   # That takes the blank space, and "RTE / NUM', out of @toplines
+
+
+### clean up notes and lines
+
+foreach $ref ( $fullsched{$schedname}{"ROUTES"},    
+               $fullsched{$schedname}{"NOTES"} ,
+               $fullsched{$schedname}{"SPECDAYS"} ) {
+   
+   grep ( s/\s//g , @$ref);
+   # eliminate spaces
+}
+
+### get {TP} information from @toplines
+
+# note: the {TP} is the short, four-character timepoint info. 
+# {TIMEPOINTS} is the long, verbal one.
+
+grep ( s/\s+$// , @toplines);
+# eliminate trailing spaces
+
+$numpoints =  1 + int ( length($toplines[0]) / 6 );
+# There are 6 characters per timepoint: a blank space in front (actually this is
+# used for two-digit times like 10:00), four characters for the point, and a space
+# in back. 
+
+# Since we've stripped the final spaces, we know that it will actually be one short
+# of the real number, so that's why the 1+ is added.
+
+
+$unpacktemplate = "A6" x $numpoints;
+# that gives the template for the unpacking. There are $numpoints points, and six characters
+# to each one.  The capital A means to strip spaces and nulls from the result.
+
+@{$fullsched{$schedname}{"TP"}} = unpack ($unpacktemplate, $toplines[0])  ;
+@tp2 =  unpack ($unpacktemplate, $toplines[1])  ;
+
+foreach (@{$fullsched{$schedname}{"TP"}}) {
+
+   $_ .= shift (@tp2); 
+   s/^\s+//;
+
+}
+
+# So now the data structure in %fullsched has the TP info.
+
+
+### parse the times
+
+foreach $thisrow (@schedrows) {
+
+   $count = 0;
+
+   foreach (unpack ($unpacktemplate, $thisrow)) {
+
+      s/^\s+//;
+      push @{$fullsched{$schedname}{"TIMES"}[$count]}, $_;
+      $count++;
+
+   }
+
+}
+
+# That should take all the times and push them to the list in the %fullsched data
+#   structure.
+
+}
+
+sub output_schedule {
+
+open TEMPFILE , ">$linenum.acs" or die "Can't open file $linenum.acs.\n";
+
+# print "Writing line $linenum to file $linenum.acs.\n";
+
+foreach $schedname (sort keys %fullsched) {
+
+   print TEMPFILE $schedname , "\n";
+   print TEMPFILE "Special Days\tNotes\tRoute\t" , join ("\t"  , @{$fullsched{$schedname}{"TIMEPOINTS"}} ) , "\n"; 
+   print TEMPFILE "SPEC DAYS\tNOTE\tRTE NUM\t" , join ("\t"  , @{$fullsched{$schedname}{"TP"}} ) , "\n"; 
+
+   # get the maximum number of rows
+   
+   
+   $maxrows = 0;
+   foreach (@{$fullsched{$schedname}{"TIMES"}}) {
+   
+   # so $_ will be the reference to the first list of times, then the ref to second list of times...
+   
+       $rowsforthispoint = scalar (@$_);
+       
+       # $_ is the reference to the list of times. @$_ is the list of times itself. scalar (@$_) is the
+       #     number of elements in the list of times. Whew!
+       
+       $maxrows = $rowsforthispoint if $rowsforthispoint > $maxrows;
+       
+   }
+
+
+   for ($i=0; $i < $maxrows ;  $i++) {
+
+      print TEMPFILE $fullsched{$schedname}{"SPECDAYS"}[$i] , "\t" ;
+      print TEMPFILE $fullsched{$schedname}{"NOTES"}[$i] , "\t" ;
+      print TEMPFILE $fullsched{$schedname}{"ROUTES"}[$i] , "\t" ;
+
+      foreach (@{$fullsched{$schedname}{TIMES}}) {
+          print TEMPFILE $_ -> [$i] , "\t";
+
+      }
+
+     # ok. $_ becomes the *reference* to the first, second, etc. list of times.  
+
+      print TEMPFILE "\n";
+
+   }
+
+   print TEMPFILE join ("\t", @{$fullsched{$schedname}{"NOTEDEFS"}} ) , "\n"; 
+   print TEMPFILE "---\n";
+
+}
+
+close TEMPFILE;
+
+}
+
+sub merge_days {
+
+my ($firstday, $secondday, $mergeday) = @_;
+
+my $count = 0;
+
+# for example, (SA, SU, WE) or (WD, WE, DA)
+
+my (@firstscheds, @secondscheds);
+
+@firstscheds = sort grep (/$firstday/ , (keys %fullsched) ) ; 
+@secondscheds = sort grep (/$secondday/ , (keys %fullsched) ) ; 
+
+# so all the ones with the first day are in @firstscheds,
+# and all the ones with the second day are in @secondscheds.
+# These are sorted so that the order of the directions are the same
+# (that is, we're comparing westbound Saturday to westbound Sunday,
+# not westbound Saturday to eastbound Sunday)
+
+return -1 unless (scalar(@firstscheds) and scalar(@secondscheds));
+return -2 unless (scalar(@firstscheds) == scalar(@secondscheds));
+
+# if there aren't any days in common, return -1
+# if there are, but the number isn't the same, return -2
+# the latter should probably never be the case -- it would mean that
+# there is an additional day in one direction. That would be weird.
+
+# I don't know that I'll actually use the return values.
+
+DAY: foreach $day (0 .. scalar(@firstscheds)) {
+
+   foreach ( qw(TP ROUTES SPECDAYS TIMES TIMEPOINTS NOTES NOTEDEFS) ) {
+
+      next DAY if scalar @{$fullsched{$firstscheds[$day]}{$_}} 
+         != scalar @{$fullsched{$secondscheds[$day]}{$_}}
+      
+   }
+   
+   # if the number of timepoints or rows, etc., are different, skip it
+   
+   foreach ( qw(TP ROUTES SPECDAYS TIMEPOINTS NOTES NOTEDEFS )) {
+   
+      next DAY 
+         if join ("" , @{$fullsched{$firstscheds[$day]}{$_}}) 
+            ne join ("" , @{$fullsched{$secondscheds[$day]}{$_}}) ;
+   }
+   
+   # if the text of any of the data is different skip it
+   
+   for ($_ =0; $_ < scalar @{$fullsched{$scheds[$day]}{"TIMES"}[0]}  ;  $_++) {
+   
+      next DAY
+         if join ("" , @{$fullsched{$scheds[$day]}{"TIMES"}[$_]})
+            ne join ("" , @{$fullsched{$scheds[$day+1]}{"TIMES"}[$_]})
+
+   }
+
+   # if any of the times are different, skip it.
+   
+
+   # At this point, we know they're identical. References make it pretty easy.
+   
+   $newschedname = $firstscheds[$day];
+   
+   $newschedname =~ s/$firstday/$mergeday/;
+   
+   $fullsched{$newschedname} = $fullsched{$firstscheds[$day]};
+   
+   # remember, that's a reference. Same reference, same thing.
+   
+   delete $fullsched{$firstscheds[$day]};
+   delete $fullsched{$secondscheds[$day]};
+   
+   # so now, the original two days are gone, but the first day is still stored
+   # in $fullsched{$newschedname}  
+   
+   $count++;
+}
+
+return $count;
+
+# returns the number of merged schedules. I don't see that it actually matters.
+
+}
