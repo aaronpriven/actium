83b48c35aadcd975a36017fd58dd0fb45996cd5f
Aaron Priven <aaron@priven.com>
2000-07-14 15:55:44 -0700

    Replacing byroutes.pl (2000-7/byroutes.pl)

--- bin/byroutes.pl
+++ bin/byroutes.pl
@@ -1,29 +1,38 @@
-sub byroutes  {
+sub byroutes ($$) {
 
-
-   no warnings "numeric";
-
-   my ($aa, $bb, $anum, $bnum);
-
-   $aa = uc($a);
-   $bb = uc($b);
-   
-   $anum = $aa > 0;
-   $bnum = $bb > 0;
+   my ($a, $b) = (uc($_[0]) , uc($_[1]));
+ 
+   my $anum = ( $a lt "A" );
+   my $bnum = ( $b lt "A" );
    # So, $anum is true if $a is a number, etc.
 
-   return ($bnum <=> $anum) unless $anum == $bnum;
+   unless ($anum == $bnum) {
+           return -1 if $anum;
+           return 1;
+   }
 
-   #  If they're not the same, return whichever one is lesser
-   #  (which will be whichever one is the number).
+   #  If they're not both numbers or both letters,
+   #  return -1 if $a is a number (and $b is not), otherwise
+   #  return 1 (since $b must be a number and $a is not)
+   
+   #  letters come after numbers in our lists.
 
-   return ($aa <=> $bb) if ($anum and $bnum);
- 
-   # if $a and $b are both numbers, return a numeric comparison
+   return ($a cmp $b) unless ($anum);
+   # return a string comparison unless they're both numeric
+   # (of course, $anum == $bnum or it would have returned already)
 
-   return ($aa cmp $bb);
+   my @a = split (/(?<=\d)(?=\D)/ , $a, 2);
+   my @b = split (/(?<=\d)(?=\D)/ , $b, 2);
 
-   # otherwise they are both strings, so return the string comparison
+   # splits on the boundary (zero-width) between
+   # a digit on the left and a non-digit on the right.
+   # so it splits 72L into 72 and L, whereas it leaves
+   # 72, O, and OX1 as one entry each.
+
+   return (   ($a[0] <=> $b[0]) || ($a[1] cmp $b[1]) )
+   # they are both numbers, so return a numeric comparison
+   # on the first component, unless they're the same, 
+   # in which case return a string comparison on the second component.
 
 }
 
