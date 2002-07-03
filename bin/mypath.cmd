d21de9cb6f85a626f7be15f5a4df4c55f3265c9f
Aaron Priven <aaron@priven.com>
2002-07-03 10:56:48 -0700

    Adding mypath.cmd (2002-08/mypath.cmd)

---
+++ bin/mypath.cmd
@@ -0,0 +1,8 @@
+
+perl c:\aarondoc\bin\strippath.pl
+
+call c:\temp\mypath.cmd
+
+SET PATH=c:\aaron\bin;c:\aarondoc\bin;U:\BIN;%PATH%;C:\Program Files\Microsoft Office\Office
+
+set PATHEXT=.gpl;.pl;%PATHEXT%
