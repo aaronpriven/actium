9572298fa1c0c3ed1b1db367cc9375a72ba139e8
Aaron Priven <aaron@priven.com>
2010-10-09 19:40:40 -0700

    Adding Linelist.pm (2011_03/Linelist.pm)

---
+++ bin/Linelist.pm
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+# vimcolor: #000030
+
+# linelist
+#
+# List lines in order by name
+# 
+
+use 5.010;
+
+use strict;
+use warnings;
+
+package Linelist;
+
+use FPMerge qw(FPread FPread_simple);
+
+use Exporter qw/import/;
+our @EXPORT_OK = qw/linelist/;
+our %EXPORT_TAGS = ( all => \@EXPORT_OK );
+
+sub linelist {
+
+   my (%idx, @idx);
+
+
+   if (@_) {
+      @idx = @_;
+   } 
+   else {
+      FPread_simple ("Skedidx.csv" , \@idx, \%idx, 'SkedID');
+   }
+
+   my %seen;
+   foreach my $idx (@idx) {
+      my @lines = split ("\c]" , $idx->{Lines});
+      $seen{$_} = 1 foreach @lines;
+   }
+
+   return keys %seen;
+
+}
