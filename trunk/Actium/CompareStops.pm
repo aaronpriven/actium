221c16b7d80aeb1ea4e98b41504403c71c7972a4
aaronpriven <aaronpriven@0dd53fb8-6e72-cf8d-f5b0-1f9b7bbc9dea>
2011-04-20 06:47:36 +0000

    first cut at completed CompareStops.pm

--- trunk/Actium/CompareStops.pm
+++ trunk/Actium/CompareStops.pm
@@ -14,17 +14,29 @@ package Actium::CompareStops 0.001;
 use Carp;
 use English ('-no_match_vars');
 
-use Actium::Sorting(qw<travelsort>);
+use Actium::Sorting(qw<sortbyline travelsort>);
 use Actium::Constants;
 use Actium::Term ('output_usage');
 use Actium::Options (qw<add_option option>);
 use Actium::Signup;
+use Actium::Util (qw<jt jn>);
 
 use Algorithm::Diff;
 
 use Actium::Patterns::Stop;
 
+use Readonly;
+
+Readonly my $DEFAULT_SKIPLINES => '399,51S,BSH';
+
 add_option( 'oldsignup', 'Previous signup to compare this signup to' );
+
+Readonly my $SKIPLINES_DESC => <<"EOT";
+Lines to skip during comparison. Separate lines with commas but without
+spaces, e.g., -skiplines 40,M,382. The default is "$DEFAULT_SKIPLINES"
+EOT
+
+add_option( 'skiplines=s', $SKIPLINES_DESC,, $DEFAULT_SKIPLINES );
 
 sub HELP {
 
@@ -47,23 +59,66 @@ HELP
     output_usage();
     return;
 
-} ## tidy end: sub HELP
+}    ## tidy end: sub HELP
 
 sub START {
 
     my $newsignup = Actium::Signup->new();
     my $oldsignup = Actium::Signup->new( { signup => option('oldsignup') } );
 
-    my ( $changes_r, $stops_of_change_r )
-      = compare_stops( $oldsignup, $newsignup );
+    my $skipped_lines_r = [ split( /,/, option('skiplines') ) ];
+
+    my $stops_row_of_r = get_xml_stops($newsignup);
+
+    my %lines_of_stop =
+      make_comparetext( $oldsignup, $newsignup, $stops_row_of_r,
+        $skipped_lines_r );
+
+    my @stopids = sort {
+        lc( $stops_row_of_r->{$a}{CityF} ) cmp
+          lc( $stops_row_of_r->{$b}{CityF} )
+          or lc( $stops_row_of_r->{$a}{OnF} ) cmp
+          lc( $stops_row_of_r->{$b}{OnF} )
+          or lc( $stops_row_of_r->{$a}{AtF} ) cmp
+          lc( $stops_row_of_r->{$b}{AtF} )
+          or $a cmp $b
+    } keys %lines_of_stop;
+
+    my $compare_folder = $newsignup->subdir('compare');
+    my $fh             = $compare_folder->open_write('comparestops.txt');
+    say $fh comparetextline_headers();
+    say $fh jn( @lines_of_stop{@stopids} );
+    close $fh or die "Can't close comparestops.txt for writing: $OS_ERROR";
 
     return;
 
 }
 
+sub get_xml_stops {
+
+    my $signup = shift;
+
+    my $xml_db = $signup->load_xml;
+    $xml_db->ensure_loaded('Stops');
+
+    emit 'Getting stop descriptions from FileMaker export';
+    my $dbh = $xml_db->dbh;
+
+    my $stops_row_of_r =
+      $xml_db->all_in_columns_key(qw/Stops CityF OnF AtF DescriptionCityF/);
+
+    emit_done;
+
+    return $stops_row_of_r;
+
+}
+
 sub compare_stops {
 
-    my ( $oldsignup, $newsignup ) = shift;
+    my ( $oldsignup, $newsignup, $skipped_lines_r ) = shift;
+
+    my %is_skipped;
+    $is_skipped{$_} = 1 foreach @{$skipped_lines_r};
 
     my $oldpatternfolder = $oldsignup->subfolder('pattern');
     my $newpatternfolder = $newsignup->subfolder('pattern');
@@ -71,39 +126,43 @@ sub compare_stops {
     my %old_stop_of = %{ $oldpatternfolder->retrieve('stops.storable') };
     my %new_stop_of = %{ $oldpatternfolder->retrieve('stops.storable') };
 
-    my @stopids = uniq( sort ( keys %new_stop_of, keys %old_stop_of ) );
+    my @stop_ids = uniq( sort ( keys %new_stop_of, keys %old_stop_of ) );
 
     my ( %changes_of, %stops_of_change );
 
   STOPID:
-    foreach my $stopid (@stopids) {
+    foreach my $stop_ids (@stop_ids) {
 
-        my @oldroutes = sort $old_stop_of{$stopid}->routes;
-        my @newroutes = sort $new_stop_of{$stopid}->routes;
+        my @oldroutes = sort
+          grep { not $is_skipped{$_} } $old_stop_of{$stop_ids}->routes;
+        my @newroutes = sort
+
+          grep { not $is_skipped{$_} } $new_stop_of{$stop_ids}->routes;
 
         next STOPID if ( "@oldroutes" eq "@newroutes" );
+
         # no changes
 
         my $result;
 
-        if ( not exists $old_stop_of{$stopid} ) {
+        if ( not exists $old_stop_of{$stop_ids} ) {
             $result = { '?' => 'AS', q{+} => \@newroutes };
         }
-        elsif ( not exists $new_stop_of{$stopid} ) {
+        elsif ( not exists $new_stop_of{$stop_ids} ) {
             $result = { '?' => 'RS', q{-} => \@oldroutes };
         }
         else {
             $result = compare_stop( \@oldroutes, \@newroutes );
         }
 
-        push @{ $stops_of_change{ $result->{'?'} } }, $stopid;
-        $changes_of{$stopid} = $result;
+        push @{ $stops_of_change{ $result->{'?'} } }, $stop_ids;
+        $changes_of{$stop_ids} = $result;
 
-    } ## tidy end: foreach my $stopid (@stopids)
+    }    ## tidy end: foreach my $stop_ids (@stop_ids)
 
     return \%changes_of, \%stops_of_change;
 
-} ## tidy end: sub compare_stops
+}    ## tidy end: sub compare_stops
 
 sub compare_stop {
     my $oldroutes_r = shift;
@@ -155,7 +214,44 @@ sub compare_stop {
 
     return \%results;
 
-} ## tidy end: sub compare_stop
+}    ## tidy end: sub compare_stop
+
+sub make_comparetext {
+    my $oldsignup       = shift;
+    my $newsignup       = shift;
+    my $stops_row_of_r  = shift;
+    my $skipped_lines_r = shift;
+
+    my ( $changes_r, $stops_of_change_r ) =
+      compare_stops( $oldsignup, $newsignup, $skipped_lines_r );
+
+    my %text_of;
+
+    while ( my ( $stop_id, $comparison_r ) = each %{$changes_r} ) {
+
+        my @columns = (
+            $comparison_r->{'?'}, $stop_id,
+            $stops_row_of_r->{PhoneID}{DescriptionCityF}
+        );
+
+        foreach my $type (qw/+ - =/) {
+            my @list = sortbyline @{ $comparison_r->{$type} };
+            push @columns, scalar @list, "@list";
+        }
+
+        $text_of{$stop_id} = jt(@columns);
+    }
+
+    return \%text_of;
+
+}
+
+sub comparetextline_headers {
+    my @columns = qw/Change Stop_ID Description
+      NumAdded Added NumRemoved Removed NumUnchanged Unchanged/;
+    return jt(@columns);
+
+}
 
 1;
 
