cb44e661d1dd0f598feb143fa565ec9120102226
Aaron Priven <aaron@priven.com>
2011-09-20 00:05:07 +0000

    Working on kidpoints/makepoints (to add Broadway Shuttle)

--- decalspec-assign.pl
+++ decalspec-assign.pl
@@ -25,39 +25,54 @@ I9          SKIP
 P10         SKIP
 SKIP        SKIP
 
-IB2         20x16-IB
-IB2N        14x16-IB
-IB3         19.5x17-IB
-IB3N        14x22-IB
-IB4N        14x26-IB
-IB5         19.5x22.25-IB
-IB5N        14x34-IB
-IB9         19.5x32.75-IB
+Field       SKIP
 
-CB15        28x35.25-CB
-CBR10       19.5x45.75-CB-R
-CBR6       19.5x35.25-CB-R
-CBR4       19.5x30-CB-R
+PBR10       SKIP
+PBR6        SKIP
+PBR2        SKIP
+PB10        SKIP
+PB15        SKIP
 
-PBO         SKIP
-
-R10         SKIP
-R6          SKIP
-R2          SKIP
-
+IC2         SKIP
 IC3         SKIP
-IC3N        SKIP
 IC4N        SKIP
 IC5         SKIP
 IC5N        SKIP
 IC9         SKIP
+IC15        SKIP
 
-PB4         SKIP
-PB6         SKIP
+CC15        28x35.25-CC
+CC4         19.5x19.5-CC
+
+PC4         SKIP
+PC6         SKIP
+PC15        SKIP
+
+IB2         SKIP
+IB2N        SKIP
+IB3         SKIP
+IB3N        SKIP
+IB4N        SKIP
+IB5         SKIP
+IB5N        SKIP
+IB9         SKIP
+
+CB15        SKIP
+CBR10       SKIP
+CBR6       SKIP
+CBR4       SKIP
 
 ) );
 
-# ignoring
+
+
+# ignoring these old sources
+
+#PBR10       20x46.25-PB-R
+#PBR6        20x35.75-PB-R
+#PBR2        20x25.25-PB-R
+#PB10        19.5x35.25-PB
+#PB15        28x35.25-PB
 
 #IC3         19.5x17-IC
 #IC3N        14x22-IC
@@ -69,6 +84,21 @@ PB6         SKIP
 #PB4         19.5x19.5-PC
 #PB6         19.5x24.75-PC
 
+# and these new ones
+
+#IB2         20x16-IB
+#IB2N        14x16-IB
+#IB3         19.5x17-IB
+#IB3N        14x22-IB
+#IB4N        14x26-IB
+#IB5         19.5x22.25-IB
+#IB5N        14x34-IB
+#IB9         19.5x32.75-IB
+#
+#CB15        28x35.25-CB
+#CBR10       19.5x45.75-CB-R
+#CBR6       19.5x35.25-CB-R
+#CBR4       19.5x30-CB-R
 
 # size - purchase source - master-or-oversize
 
