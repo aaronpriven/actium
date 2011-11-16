92bb9bf16f96a52c9abccb3ad2d072eb63c2e984
aaronpriven@gmail.com <aaronpriven@gmail.com@0dd53fb8-6e72-cf8d-f5b0-1f9b7bbc9dea>
2011-11-16 20:12:24 +0000

    Added st.pl for searching stop names, and iphoto_stops.pl for geotagging iphoto pictures

---
+++ trunk/st.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/perl
+
+use 5.010;
+
+use strict;
+use warnings;
+
+my $simplefile = '/volumes/bireme/actium/db/f11/SimpleStops.tab';
+
+open my $in, '<', $simplefile
+  or die "Can't open $simplefile";
+
+$/ = "\r";    # FileMaker exports CRs
+
+use constant { PHONEID => 0, STOPID => 1, DESC => 2, LAT => 3, LONG => 4 };
+
+my ( %of_phoneid, %of_stopid );
+
+die "Usage: $0 [-n] id [ id ] ...\nid can be a phoneid or stopid.\n"
+  unless @ARGV;
+
+while (<$in>) {
+
+    chomp;
+    next unless $_;
+    my @fields  = split(/\t/);
+    my $stopid  = $fields[STOPID];
+    my $phoneid = $fields[PHONEID];
+    $of_phoneid{$phoneid} = \@fields;
+    $of_stopid{$stopid}   = \@fields;
+}
+
+my $dump = 0;
+
+close $in
+  or die "Can't open $simplefile";
+
+foreach (@ARGV) {
+    if ( $_ eq "-d" ) {
+        $dump = 1;
+        next;
+    }
+
+    if (/\A\d{5}\z/) {
+        display( $of_phoneid{$_} );
+        next;
+    }
+
+    if (/\A\d{6}\z/) {
+        display( $of_stopid{"0$_"} );
+        next;
+    }
+
+    if (/\A\d{7,8}\z/) {
+        display( $of_stopid{"$_"} );
+        next;
+    }
+
+    if ($dump) {
+            say "0||||";
+    } else {
+       #warn "Unknown id type: $_\n";
+      foreach my $fields_r (values %of_stopid) {
+       
+          my $desc = $fields_r ->[DESC];
+          display($fields_r)  if $desc =~ /$_/i;
+       
+      } 
+       
+       
+       
+       
+    } 
+
+} ## tidy end: foreach (@ARGV)
+
+sub display {
+ 
+    my $fields_r = shift;
+    if ( ref($fields_r) ne 'ARRAY' ) {
+        if ($dump) {
+            say "0||||";
+        } else {
+        say "Unknown id $_";
+        }
+        return;
+    }
+    if ($dump) {
+        say( join( q{|}, @{$fields_r} ) );
+
+    }
+    else {
+        print ( $fields_r->[PHONEID], '  ', $fields_r->[STOPID] , '  ' );
+        say( $fields_r->[DESC] );
+    }
+
+}
