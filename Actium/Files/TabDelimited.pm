d8518f4aac5446ac2936f1f9a16a735d0c06f267
Aaron Priven <aaron@priven.com>
2012-02-01 00:53:23 +0000

    If all you have is a Moose then everything looks like an antler. Or something like that. This is a rewrite of Actium::Files::TabDelimited to be procedural, using a callback for each line, rather than object-oriented, using an object call for each line. Should be much better.

--- Actium/Files/TabDelimited.pm
+++ Actium/Files/TabDelimited.pm
@@ -1,7 +1,6 @@
 # Actium/Files/TabDelimited.pm
 
-# Class for reading Tab-Delimited files and returning them,
-# line by line, to the caller.
+# Class for reading Tab-Delimited files and returning them to the caller.
 
 # Subversion: $Id$
 
@@ -10,237 +9,128 @@ use 5.014;    # turns on features
 
 package Actium::Files::TabDelimited 0.001;
 
-use Moose;
-use MooseX::SemiAffordanceAccessor;
-use MooseX::StrictConstructor;
-
 use Carp;
 use English '-no_match_vars';
 use Text::Trim;
+
+use Params::Validate;
 
 use Actium::Signup;
 use Actium::Term;
 use Actium::Util('filename');
 
 use Readonly;
-
 Readonly my $LINES_BETWEEN_EMITTING_PERCENTAGES => 2000;
 
-has folder => (
-    is       => 'ro',
-    isa      => 'Actium::Signup',
-    required => 1,
-);
+use Sub::Exporter -setup => { exports => [ qw(read_tab_files) ] };
 
-has _passed_files_r => (
-    is       => 'ro',
-    isa      => 'ArrayRef[Str]',
-    init_arg => 'files',
-    traits   => ['Array'],
-    default  => sub { [] },
-    handles  => { _passed_files => 'elements', },
-);
+sub read_tab_files {
 
-has glob_files => (
-    is       => 'ro',
-    isa      => 'ArrayRef[Str]',
-    init_arg => 'glob_files',
-    traits   => ['Array'],
-    default  => sub { [] },
-    handles  => { _globpatterns => 'elements', },
-);
+    my %params = validate(
+        @_,
+        {   folder => {
+                can => [
+                    'make_filespec', 'glob_plain_files',
+                    'open_read',     'display_path'
+                ]
+            },
+            # eventually replace isa with can
+            files            => { type => 'ARRAYREF', default => [] },
+            globpatterns     => { type => 'ARRAYREF', default => [] },
+            required_headers => { type => 'ARRAYREF', default => [] },
+            callback         => { type => 'CODEREF' },
+        },
 
-has _files_r => (
-    isa     => 'ArrayRef[Str]',
-    traits  => ['Array'],
-    handles => {
-        _files     => 'elements',
-        _next_file => 'shift',
-    },
-    lazy    => 1,
-    builder => '_build_files',
-);
+    );
 
-sub _build_files {
-    my $self   = shift;
-    my $folder = $self->folder;
-    my @files  = $self->_passed_files;
-    my @globs  = $self->_globpatterns;
+    my $folder             = $params{folder};
+    my $passed_files_r     = $params{files};
+    my $globpatterns_r     = $params{globpatterns};
+    my $required_headers_r = $params{required_headers};
+    my $callback_r         = $params{callback};
 
-    foreach (@files) {
-        $_ = $folder->make_filespec($_);
+    my @files = _expand_files( $passed_files_r, $globpatterns_r, $folder );
+
+    foreach my $file (@files) {
+
+        emit "Loading $file";
+
+        my $fh = $folder->open_read($file);
+
+        my @headers = _verify_headers( $fh, $file, $required_headers_r );
+
+        my $size    = -s $file;
+        my $linenum = 0;
+
+        emit_over ' 0%';
+
+        while ( my $line = <$fh> ) {
+
+            $linenum++;
+            if ( not $linenum % $LINES_BETWEEN_EMITTING_PERCENTAGES ) {
+                emit_over( sprintf( ' %.0f%%', tell($fh) / $size * 100 ) );
+            }
+
+            my %value_of;
+            @value_of{@headers} = trim( split( "\t", $line ) );
+
+            $callback_r->( \%value_of );
+
+        }
+
+        emit_over ' 100%';
+
+        close $fh or croak "Can't close $file for writing";
+
+        emit_done;
+
+    } ## tidy end: foreach my $file (@files)
+
+} ## tidy end: sub read_tab_files
+
+sub _expand_files {
+
+    my ( $files_r, $globpatterns_r, $folder ) = @_;
+
+    my @files;
+
+    foreach ( @{$files_r} ) {
+        push @files, $folder->make_filespec($_);
     }
 
-    foreach (@globs) {
-        push @files, $folder->glob_plain_files($_);
+    foreach (@$globpatterns_r) {
+        push @files, filename( $folder->glob_plain_files($_) );
     }
 
     if ( not scalar @files ) {
         my $path = $folder->display_path;
-        emit_text( "No files found passed to " . __PACKAGE__ );
-        emit_error;
-        croak 'Files not found';
-
+        croak("No files found in $path passed to read_tab_files ");
     }
 
-    return \@files;
-} ## tidy end: sub _build_files
+    return @files;
 
-has required_headers_r => (
-    traits   => ['Array'],
-    init_arg => 'required_headers',
-    isa      => 'ArrayRef[Str]',
-    is       => 'ro',
-    required => 0,
-    default  => sub { [] },
-    handles  => { required_headers => 'elements' },
-);
+} ## tidy end: sub _expand_files
 
-has _fh => (
-    is      => 'rw',
-    isa     => 'FileHandle',
-    lazy    => 1,
-    builder => '_first_fh',
-);
+sub _verify_headers {
 
-has _current_file => (
-    is  => 'rw',
-    isa => 'Str',
-);
+    my $fh               = shift;
+    my $file             = shift;
+    my @required_headers = @{ +shift };
 
-has _linenum => (
-    traits  => ['Counter'],
-    is      => 'bare',
-    isa     => 'Int',
-    default => 0,
-    handles => {
-        _inc_linenum   => 'inc',
-        _reset_linenum => 'reset',
-    },
-);
+    my $headerline = trim( scalar(<$fh>) );
+    my @headers = split( "\t", $headerline );
 
-has _size => (
-    is  => 'rw',
-    isa => 'Int',
-);
-
-sub _first_fh {
-    my $self = shift;
-    my $fh   = $self->_open_fh();
-    return $fh;
-}
-
-sub _next_fh {
-    my $self         = shift;
-    my $fh           = $self->_fh;
-    my $current_file = $self->_current_file;
-    emit_over "$current_file: 100%";
-    emit_done;
-
-    $self->close($fh);
-
-    $fh = $self->_open_fh;
-    return unless defined $fh;
-
-    $self->_set_fh($fh);
-    return $fh;
-}
-
-sub _open_fh {
-    my $self = shift;
-    my $file = $self->_next_file;
-
-    return if not defined $file;
-
-    my $current_file = filename($file);
-    $self->_set_current_file($current_file);
-
-    my $result = open my $fh, '<:encoding(UTF-8)', $file;
-
-    if ( not $result ) {
-        emit_error;
-        croak "Can't open $file for reading: $OS_ERROR";
-    }
-
-    my $size = -s $file;
-    $self->_set_size($size);
-    $self->_reset_linenum;
-
-    emit_over '$current_file: 0%';
-
-    my $line = trim( scalar(<$fh>) );
-    my @headers = split( "\t", $line );
-
-    my @required_headers = $self->required_headers;
     if ( scalar @required_headers ) {
         foreach my $required_header (@required_headers) {
             if ( not $required_header ~~ @headers ) {
-                emit_text
+                croak
                   "Required header $required_header not found in file $file";
-                emit_error;
-                croak 'Required header not found';
             }
         }
     }
 
-    $self->_set_headers_r( \@headers );
+    return @headers;
 
-    return $fh;
-
-} ## tidy end: sub _open_fh
-
-has '_headers_r' => (
-    # set by _next_fh
-    traits  => ['Array'],
-    is      => 'rw',
-    handles => { _headers => 'elements' },
-    isa     => 'ArrayRef[Str]',
-);
-
-sub next_line {
-    my $self         = shift;
-    my $fh           = $self->_fh;
-    my $current_file = $self->_current_file;
-
-    my $line;
-
-    {
-        return if not defined $fh;
-        $line = readline($fh);
-        if ( not defined $line ) {
-            $fh = $self->_next_fh;
-            redo;
-        }
-    }
-
-    my $linenum = $self->_inc_linenum;
-    if ( not $linenum % $LINES_BETWEEN_EMITTING_PERCENTAGES ) {
-        emit_over( $current_file
-              . sprintf( ': %.0f%%', tell($fh) / $self->_size * 100 ) )
-          ;
-    }
-
-    my @headers = $self->_headers;
-    my %value_of;
-    @value_of{@headers} = trim( split( "\t", $line ) );
-
-    return \%value_of;
-} ## tidy end: sub next_line
-
-sub close {
-    my $self   = shift;
-    my $fh     = $self->_fh;
-    my $result = close $fh;
-
-    if ( not $result ) {
-        my $file = $self->_current_file;
-        emit_error;
-        croak "Can't close file for reading: $OS_ERROR";
-    }
-
-}
-
-no Moose;
-__PACKAGE__->meta->make_immutable;    ## no critic (RequireExplicitInclusion)
+} ## tidy end: sub _verify_headers
 
 __END__
