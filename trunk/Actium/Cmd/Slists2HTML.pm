2a3cbf0e7678707ede34809c10e752e0d451d333
aaronpriven@gmail.com <aaronpriven@gmail.com@0dd53fb8-6e72-cf8d-f5b0-1f9b7bbc9dea>
2012-09-25 21:54:18 +0000

    Updates for DB, notably listed cities and added Google Map links

--- trunk/Actium/Cmd/Slists2HTML.pm
+++ trunk/Actium/Cmd/Slists2HTML.pm
@@ -17,6 +17,8 @@ use Actium::Util('filename');
 use Actium::Term;
 use English '-no_match_vars';
 
+use HTML::Entities;
+
 sub HELP {
     say 'No help yet, sorry.';
 }
@@ -30,20 +32,19 @@ sub START {
     my $stoplists_folder      = $signup->subfolder('slists');
     my $stoplists_line_folder = $stoplists_folder->subfolder('line');
     #    my $linehtml_folder       = $stoplists_folder->subfolder('linehtml');
-    
-    
+
     my $xml_db = $signup->load_xml;
-   $xml_db->ensure_loaded('Stops');
-   
+    $xml_db->ensure_loaded('Stops');
+
     emit 'Getting stop descriptions from FileMaker export';
 
     my $dbh = $xml_db->dbh;
 
-    my $stops_row_of_r =
-      $xml_db->all_in_columns_key(qw/Stops DescriptionListF/);
+    my $stops_row_of_r
+      = $xml_db->all_in_columns_key(qw/Stops DescriptionListF GoogleURL CityF/);
 
     emit_done;
-    
+
     emit "Creating HTML versions of stop lists";
 
     my @files = $stoplists_line_folder->glob_plain_files('*.txt');
@@ -59,14 +60,14 @@ sub START {
     my %table_of;
     my %tables_of_type;
     my %routes_of_type;
-    
+
     foreach my $route ( sortbyline keys %dirs_of ) {
-     
+
         next if $LINE_SHOULD_BE_SKIPPED{$route};
-     
+
         emit_over $route;
-        
-        my @dirs = @{$dirs_of{$route}};
+
+        my @dirs = @{ $dirs_of{$route} };
         @dirs = sort { $order_of{$a} <=> $order_of{$b} } @dirs;
         my %stops_of;
         my %stoplines_of;
@@ -78,42 +79,61 @@ sub START {
 
             my $headerline = readline($ifh);    # thrown away
 
+            my $prevcity = $EMPTY_STR;
+
             while ( defined( my $stopline = readline($ifh) ) ) {
                 chomp $stopline;
                 my ( $stopid, $scheduling_desc ) = split( /\t/, $stopline );
-                
+
                 next if $scheduling_desc =~ /^Virtual/;
-                
-                my $desc = $stops_row_of_r->{$stopid}{DescriptionListF};
-                
-                push @{ $stoplines_of{$dir} }, $stopid . ' =&gt; ' . $desc;
-                
-                push @{ $stops_of{$dir} },     $stopid;
+
+                my $desc = encode_entities(
+                    $stops_row_of_r->{$stopid}{DescriptionListF} );
+                my $url  = $stops_row_of_r->{$stopid}{GoogleURL};
+                my $city = encode_entities($stops_row_of_r->{$stopid}{CityF});
+
+                my $citytext = $EMPTY_STR;
+
+                if ( $prevcity ne $city ) {
+                    $citytext
+                      = '<span style="text-decoration: underline; font-weight: bold;">'
+                      . $city
+                      . '</span><br />';
+                    $citytext = '</p><p>' . $citytext if $prevcity;
+
+                    $prevcity = $city;
+                }
+
+                #push @{ $stoplines_of{$dir} }, $stopid . ' =&gt; ' . $desc;
+                push @{ $stoplines_of{$dir} }, 
+                    $citytext .
+                      qq{$desc (<a href="$url" target="_blank">$stopid</a>)}
+                ;
+
+                push @{ $stops_of{$dir} }, $stopid;
                 #my $savedline = $stopline =~ s/\t/ =&gt; /r;
                 #push @{ $stoplines_of{$dir} }, $savedline;
-            }
+            } ## tidy end: while ( defined( my $stopline...))
 
             close $ifh or die "Can't close $file: $OS_ERROR";
 
-        }
+        } ## tidy end: foreach my $dir (@dirs)
 
-        my @dir_objs = map { Actium::Sked::Dir->new($_) } @dirs;
+        my @dir_objs  = map { Actium::Sked::Dir->new($_) } @dirs;
         my @dir_bound = map { $_->as_bound } @dir_objs;
-        
+
         # make dummy stop list if there's only one direction
         if ( @dirs == 1 ) {
             push @dirs, $EMPTY_STR;
-            $stops_of{$EMPTY_STR} = [$EMPTY_STR];
+            $stops_of{$EMPTY_STR}     = [$EMPTY_STR];
             $stoplines_of{$EMPTY_STR} = [$EMPTY_STR];
             push @dir_bound, $EMPTY_STR;
-            
+
         }
 
         my $outdata;
         open( my $ofh, '>:utf8', \$outdata )
           or die "Can't open memory location as file: $OS_ERROR";
-          
-
 
         say $ofh <<"EOT";
 <h3><a name="$route">$route</a></h3>
@@ -125,9 +145,9 @@ sub START {
 EOT
 
         for my $dir (@dirs) {
-            say $ofh q{<td valign=top>};
+            say $ofh q{<td valign=top><p>};
             say $ofh join( "<br />", @{ $stoplines_of{$dir} } );
-            say $ofh '</td>';
+            say $ofh '</p></td>';
         }
 
         say $ofh "</tr></tbody></table>";
@@ -144,6 +164,9 @@ EOT
             when ('800') {
                 $type = 'Transbay';
             }
+            when (/^DB/) {
+                $type = 'Dumbarton Express';
+            }
             when (/^[A-Z].*/) {
                 $type = 'Transbay';
             }
@@ -156,50 +179,36 @@ EOT
             default {
                 $type = 'Local';
             }
-        }
+        } ## tidy end: given
 
-        push @{ $routes_of_type{$type} } , $route;
+        push @{ $routes_of_type{$type} }, $route;
         push @{ $tables_of_type{$type} }, $outdata;
 
     } ## tidy end: foreach my $route ( sortbyline...)
-    
-    foreach my $type (keys %tables_of_type) {
-     
+
+    foreach my $type ( keys %tables_of_type ) {
+
         my $ofh = $stoplists_folder->open_write("$type.html");
-        
+
         say $ofh '<table border="0" cellspacing="0" cellpadding="10">';
         say $ofh '<tr>';
-        
-        my @routes = @{$routes_of_type{$type}};
-        
-        for my $i ( 0 .. $#routes) {
+
+        my @routes = @{ $routes_of_type{$type} };
+
+        for my $i ( 0 .. $#routes ) {
             my $route = $routes[$i];
             print $ofh "<td><a href='#$route'>$route</a></td>";
-            if ( not (($i+1) % 10) ) {
+            if ( not( ( $i + 1 ) % 10 ) ) {
                 print $ofh "</tr>\n<tr>";
             }
-         
+
         }
         say $ofh "</tr></table>";
-        
-        say $ofh join ("\n" , @{$tables_of_type{$type}});
+
+        say $ofh join( "\n", @{ $tables_of_type{$type} } );
         close $ofh or die "Can't close $type.html: $OS_ERROR";
-    }
-    
+    } ## tidy end: foreach my $type ( keys %tables_of_type)
+
     emit_done;
-        
-        
+
 } ## tidy end: sub START
-
-__END__
-
-<h3><a name="Emeryville"></a>Emeryville</h3>
-<table border="1" cellspacing="0" cellpadding="6"><colgroup> <col width="350ex" /> <col width="250ex" /></colgroup>
-<tbody>
-<tr>
-<td valign="top">Emery Secondary School</td>
-<td><a href="/maps/schedule_results.php?quick_line=72&amp;Go=Go">72</a>   <a href="/maps/schedule_results.php?quick_line=72M&amp;Go=Go">72M</a>   <a href="/maps/schedule_results.php?quick_line=72R&amp;Go=Go">72R</a></td>
-</tr>
-</tbody>
-</table>
-
