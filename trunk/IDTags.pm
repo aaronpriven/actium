ad506126b535d106a9b748b911ee09f0c096d1a1
aaronpriven@gmail.com <aaronpriven@gmail.com@0dd53fb8-6e72-cf8d-f5b0-1f9b7bbc9dea>
2013-08-26 20:40:17 +0000

    Replaced Perl6::Export::Attrs with Sub::Exporter

--- trunk/IDTags.pm
+++ trunk/IDTags.pm
@@ -9,19 +9,21 @@ package IDTags;
 
 use strict;
 
-use Perl6::Export::Attrs;
-
-#our (@ISA ,@EXPORT_OK ,%EXPORT_TAGS);
-
-#use Exporter;
-#@ISA = ('Exporter');
-#@EXPORT_OK = qw(start);
+use Sub::Exporter -setup => {
+    exports => [
+        qw<start start_with_tables underline bold parastyle charstyle 
+           nocharstyle dropcapchars punctuationspace thinspace bullet 
+           boxbreak superscript nbsp endash emdash softreturn color 
+           emspace enspace nonjoiner  thirdspace hairspace discretionary_lf 
+           combiside combifootnote combichar>
+    ]
+};
 
 use Carp;
 
-sub start :Export { 
+sub start {
 
-my $starttext = <<'EOF';
+    my $starttext = <<'EOF';
 <ASCII-MAC>
 <Version:2.000000><FeatureSet:InDesign-Roman><ColorTable:=<Black:COLOR:CMYK:Process:0.000000,0.000000,0.000000,1.000000><Grey20:COLOR:CMYK:Process:0.000000,0.000000,0.000000,0.200000><Grey80:COLOR:CMYK:Process:0.000000,0.000000,0.000000,0.800000><H3-Blue:COLOR:CMYK:Process:1.000000,0.300000,0.000000,0.000000><H8-Blue:COLOR:CMYK:Process:0.900000,0.050000,0.000000,0.000000><H12-Aqua:COLOR:CMYK:Process:0.600000,0.000000,0.150000,0.000000><H15-Green:COLOR:CMYK:Process:0.750000,0.000000,0.400000,0.000000><H16-Green:COLOR:CMYK:Process:0.700000,0.000000,0.800000,0.000000><H43-Aqua:COLOR:CMYK:Process:0.500000,0.000000,0.351000,0.000000><H47-Blue:COLOR:CMYK:Process:0.650000,0.250000,0.000000,0.000000><H51-Blue:COLOR:CMYK:Process:0.600000,0.150000,0.000000,0.000000><H69-Green:COLOR:CMYK:Process:0.351000,0.000000,0.351000,0.000000><H71-Purple:COLOR:CMYK:Process:0.351000,0.500000,0.000000,0.000000><H81-Green:COLOR:CMYK:Process:0.351000,0.000000,0.700000,0.000000><H101-Purple:COLOR:CMYK:Process:0.550000,0.600000,0.000000,0.000000><H103-Pink:COLOR:CMYK:Process:0.030000,0.351000,0.000000,0.000000><H108-Yellow:COLOR:CMYK:Process:0.150000,0.000000,0.800000,0.000000><H121-Orange:COLOR:CMYK:Process:0.000000,0.150000,0.800000,0.050000><H122-Pink:COLOR:CMYK:Process:0.150000,0.550000,0.000000,0.000000><H124-Pink:COLOR:CMYK:Process:0.000000,0.500000,0.150000,0.000000><H125-Pink:COLOR:CMYK:Process:0.000000,0.600000,0.351000,0.000000><H130-Orange:COLOR:CMYK:Process:0.000000,0.400000,0.800000,0.000000><H132-Rose:COLOR:CMYK:Process:0.000000,0.700000,0.300000,0.000000><H134-Magenta:COLOR:CMYK:Process:0.100000,0.700000,0.000000,0.000000><H135-Red:COLOR:CMYK:Process:0.050000,0.800000,0.750000,0.000000><H136-Red:COLOR:CMYK:Process:0.150000,1.000000,0.650000,0.000000>>
 <DefineParaStyle:Normal=<Nextstyle:Normal><cTypeface:57 Condensed><cSize:10.000000><cAutoPairKern:Optical><cLigatures:0><cBaselineShift:-0.000000><pHyphenationLadderLimit:0><pAutoLeadPercent:1.204987><cLeading:12.000000><pMinCharBeforeHyphen:3><pShortestWordHyphenated:6><pHyphenationZone:0.000000><cFont:Univers><pMaxWordSpace:2.500000><pMinWordSpace:0.750000><pMaxLetterspace:0.039993><cHang:Baseline><pRuleAboveColor:Black><pRuleAboveTint:100.000000><pRuleBelowColor:Black><pRuleBelowTint:100.000000>>
@@ -37,20 +39,20 @@ my $starttext = <<'EOF';
 <DefineCharStyle:Bold=<Nextstyle:Bold><cTypeface:67 Bold Condensed>><DefineCharStyle:footnum=<Nextstyle:footnum>>
 EOF
 
-$starttext =~ s/\n/\r/g; #CRs rather than LFs for Mac programs
+    $starttext =~ s/\n/\r/g;    #CRs rather than LFs for Mac programs
 
-$starttext =~ s/\r$//; # strip trailing newline
+    $starttext =~ s/\r$//;      # strip trailing newline
 
-return $starttext;
+    return $starttext;
 
-# these are the definitions out of a test tagged file. The specific values
-# are intended to be overridden by the InDesign import routine.
+    # these are the definitions out of a test tagged file. The specific values
+    # are intended to be overridden by the InDesign import routine.
 
-}
+} ## tidy end: sub start :
 
-sub start_with_tables :Export { 
+sub start_with_tables  {
 
-my $starttext = <<'EOF';
+    my $starttext = <<'EOF';
 <ASCII-MAC>
 <Version:2.000000><FeatureSet:InDesign-Roman><ColorTable:=<Black:COLOR:CMYK:Process:0.000000,0.000000,0.000000,1.000000><Grey20:COLOR:CMYK:Process:0.000000,0.000000,0.000000,0.200000><Grey80:COLOR:CMYK:Process:0.000000,0.000000,0.000000,0.800000><H3-Blue:COLOR:CMYK:Process:1.000000,0.300000,0.000000,0.000000><H8-Blue:COLOR:CMYK:Process:0.900000,0.050000,0.000000,0.000000><H12-Aqua:COLOR:CMYK:Process:0.600000,0.000000,0.150000,0.000000><H15-Green:COLOR:CMYK:Process:0.750000,0.000000,0.400000,0.000000><H16-Green:COLOR:CMYK:Process:0.700000,0.000000,0.800000,0.000000><H43-Aqua:COLOR:CMYK:Process:0.500000,0.000000,0.351000,0.000000><H47-Blue:COLOR:CMYK:Process:0.650000,0.250000,0.000000,0.000000><H51-Blue:COLOR:CMYK:Process:0.600000,0.150000,0.000000,0.000000><H69-Green:COLOR:CMYK:Process:0.351000,0.000000,0.351000,0.000000><H71-Purple:COLOR:CMYK:Process:0.351000,0.500000,0.000000,0.000000><H81-Green:COLOR:CMYK:Process:0.351000,0.000000,0.700000,0.000000><H101-Purple:COLOR:CMYK:Process:0.550000,0.600000,0.000000,0.000000><H103-Pink:COLOR:CMYK:Process:0.030000,0.351000,0.000000,0.000000><H108-Yellow:COLOR:CMYK:Process:0.150000,0.000000,0.800000,0.000000><H121-Orange:COLOR:CMYK:Process:0.000000,0.150000,0.800000,0.050000><H122-Pink:COLOR:CMYK:Process:0.150000,0.550000,0.000000,0.000000><H124-Pink:COLOR:CMYK:Process:0.000000,0.500000,0.150000,0.000000><H125-Pink:COLOR:CMYK:Process:0.000000,0.600000,0.351000,0.000000><H130-Orange:COLOR:CMYK:Process:0.000000,0.400000,0.800000,0.000000><H132-Rose:COLOR:CMYK:Process:0.000000,0.700000,0.300000,0.000000><H134-Magenta:COLOR:CMYK:Process:0.100000,0.700000,0.000000,0.000000><H135-Red:COLOR:CMYK:Process:0.050000,0.800000,0.750000,0.000000><H136-Red:COLOR:CMYK:Process:0.150000,1.000000,0.650000,0.000000>>
 <DefineParaStyle:Normal=<Nextstyle:Normal><cTypeface:57 Condensed><cSize:10.000000><cAutoPairKern:Optical><cLigatures:0><cBaselineShift:-0.000000><pHyphenationLadderLimit:0><pAutoLeadPercent:1.204987><cLeading:12.000000><pMinCharBeforeHyphen:3><pShortestWordHyphenated:6><pHyphenationZone:0.000000><cFont:Univers><pMaxWordSpace:2.500000><pMinWordSpace:0.750000><pMaxLetterspace:0.039993><cHang:Baseline><pRuleAboveColor:Black><pRuleAboveTint:100.000000><pRuleBelowColor:Black><pRuleBelowTint:100.000000>>
@@ -70,147 +72,146 @@ my $starttext = <<'EOF';
 <DefineCharStyle:Bold=<Nextstyle:Bold><cTypeface:67 Bold Condensed>>
 EOF
 
-$starttext =~ s/\n/\r/g; #CRs rather than LFs for Mac programs
+    $starttext =~ s/\n/\r/g;    #CRs rather than LFs for Mac programs
 
-$starttext =~ s/\r$//; # strip trailing newline
+    $starttext =~ s/\r$//;      # strip trailing newline
 
-return $starttext;
+    return $starttext;
 
-# these are the definitions out of a test tagged file. The specific values
-# are intended to be overridden by the InDesign import routine.
+    # these are the definitions out of a test tagged file. The specific values
+    # are intended to be overridden by the InDesign import routine.
+
+} ## tidy end: sub start_with_tables :
+
+sub underline {
+    my $text = shift;
+    return "<CharStyle:Underline>$text<CharStyle:>";
+}
+
+sub bold {
+    my $text = shift;
+    return "<CharStyle:Bold>$text<CharStyle:>";
+}
+
+sub parastyle {
+
+    my ( $style, @text ) = @_;
+
+    my $text = join( "", @text );
+
+    return "<ParaStyle:$style>$text";
 
 }
 
-sub underline :Export { 
-    my $text = shift; 
-    return "<CharStyle:Underline>$text<CharStyle:>" ;
+sub charstyle {
+    my ( $style, @text ) = @_;
+    my $text = join( "", @text );
+    return "<CharStyle:$style>$text";
 }
 
-sub bold :Export { 
-    my $text = shift; 
-    return "<CharStyle:Bold>$text<CharStyle:>" ;
+sub nocharstyle {
+    return "<CharStyle:>";
 }
 
-sub parastyle :Export {
-
-   my ($style, @text) = @_;
-
-   my $text = join("", @text);
-
-   return "<ParaStyle:$style>$text";
-
+sub dropcapchars {
+    my $chars = shift;
+    return "<pdcc:$chars>";
 }
 
-sub charstyle :Export {
-   my ($style, @text) = @_;
-   my $text = join("", @text);
-   return "<CharStyle:$style>$text";
+sub punctuationspace { return '<0x2008>' }
+
+sub thinspace { return '<0x2009>' }
+
+sub bullet { return '<0x2022>' }
+
+sub boxbreak {
+    return "<cNextXChars:Box>\r<cNextXChars:>";
 }
 
-sub nocharstyle :Export {
-   return "<CharStyle:>";
+sub superscript {
+    return "<cPosition:Superscript>@_<cPosition:>";
 }
 
-sub dropcapchars :Export {
-   my $chars = shift;
-   return "<pdcc:$chars>";
+sub nbsp {
+    return '<0x00A0>';
 }
 
-sub punctuationspace :Export {return '<0x2008>' }
-
-sub thinspace :Export {return '<0x2009>' }
-
-sub bullet :Export { return '<0x2022>' }
-
-sub boxbreak :Export {
-   return "<cNextXChars:Box>\r<cNextXChars:>";
+sub endash {
+    return '<0x2013>';
 }
 
-sub superscript :Export {
-   return "<cPosition:Superscript>@_<cPosition:>";
+sub emdash {
+    return '<0x2014>';
 }
 
-sub nbsp :Export {
-   return '<0x00A0>';
+sub softreturn {
+    return "\n";    # this really is an \n, as opposed to the usual \r
 }
 
-sub endash :Export {
-   return '<0x2013>';
+sub color {
+    my ( $color, @text ) = @_;
+    my $text = join( "", @text );
+    return "<cColor:$color>$text<cColor:>";
 }
 
-sub emdash :Export {
-   return '<0x2014>';
-}
- 
-sub softreturn :Export {
-    return "\n"; # this really is an \n, as opposed to the usual \r
+sub emspace {
+    return '<0x2003>';
 }
 
-sub color :Export {
-   my ($color, @text) = @_;
-   my $text = join("", @text);
-   return "<cColor:$color>$text<cColor:>";
+sub enspace {
+    return '<0x2002>';
 }
 
-sub emspace :Export {
-   return '<0x2003>';
+sub nonjoiner {
+    return '<0x200C>';
 }
 
-sub enspace :Export {
-   return '<0x2002>';
+sub thirdspace {
+    return '<0x2004>';
 }
 
-sub nonjoiner  :Export {
-   return '<0x200C>';
+sub hairspace {
+    return '<0x200A>';
 }
 
-sub thirdspace :Export {
-   return '<0x2004>';
+sub discretionary_lf {
+    return '<0x200B>';
 }
 
-sub hairspace :Export {
-   return '<0x200A>';
+sub combiside {
+    my $num = combichar( +shift );
+    return charstyle( 'sidenum', $num ) . nocharstyle;
 }
 
-
-sub discretionary_lf :Export {
-   return '<0x200B>';
+sub combifootnote {
+    my $num = combichar( +shift );
+    return charstyle( 'footnum', $num ) . nocharstyle;
 }
 
-sub combiside :Export {
-   my $num = combichar ( +shift);
-   return charstyle ('sidenum' , $num) . nocharstyle;
-}
+sub combichar {
 
-sub combifootnote :Export {
-   my $num = combichar ( +shift);
-   return charstyle ('footnum' , $num) . nocharstyle;
-}
+    my $num = shift;
 
-sub combichar :Export {
+    if ( $num < 1 or $num > 99 or $num != int($num) ) {
+        croak "invalid footnote '$num'";
+    }
 
-   my $num = shift;
+    if ( $num < 20 ) {
+        $num = (qw(p q w e r t y u i o a s d f g h j k l ;))[$num];
+        # 1 through 19
+    }
+    else {
+        my @chars = split( //, $num );
+        no warnings qw(qw);
+        $chars[1] = (qw/ ) ! @ # $ % ^ & * ( /)[ $chars[1] ];
+        # The characters above are the right halves of two-digit numbers.
+        # 0-9 are, themselves, the left halves of two-digit numbers,
+        # so we don't need to modify those.
+        $num = join( '', @chars );
+    }
 
-   if ( $num < 1 or $num > 99 or $num != int($num) ) { 
-        croak "invalid footnote '$num'" ;
-   }
+    return $num;
 
-   if ($num < 20) {
-      $num = (qw(p q w e r t y u i o a s d f g h j k l ;))[$num];
-      # 1 through 19
-   }
-   else {
-       my @chars = split (// , $num);
-       no warnings qw(qw);
-       $chars[1] = (qw/ ) ! @ # $ % ^ & * ( /)[$chars[1]];
-       # The characters above are the right halves of two-digit numbers.
-       # 0-9 are, themselves, the left halves of two-digit numbers,
-       # so we don't need to modify those.
-       $num = join('', @chars);
-   }
-
-   return $num;
-   
-}
+} ## tidy end: sub combichar :
 
 1;
