17f3f991ac675c0ea8ae63e3d918032410787c84
Aaron Priven <aaron@priven.com>
2013-10-09 03:27:40 +0000

    Updated Preamble, added Actium::Moose

--- Actium/Constants.pm
+++ Actium/Constants.pm
@@ -8,17 +8,13 @@
 use strict;
 use warnings;
 
-package Actium::Constants 0.002;
+package Actium::Constants 0.003;
+# Cannot use Actium::Preamble since that module depends on this one
 
-use 5.014;
-
-# Constants.pm
-# ACTium shared constants
-# The scalars are read-only and will create an error
-# if they are modified. Sadly, not so with hashes or arrays
+use 5.016;
+use Const::Fast;
 
 my %constants;
-my ( $name, $value );
 
 ## no critic (ProhibitMagicNumbers)
 
@@ -38,8 +34,6 @@ BEGIN {
 
         LINES_TO_COMBINE => {
             '72M' => '72',
-#            'DB1' => 'DB',
-#            'DB3' => 'DB',
             '386' => '86',
             'NC'  => 'NX4',
             'NXC' => 'NX4',
@@ -51,22 +45,9 @@ BEGIN {
         # Weekdays-and-Sundays, Weekends, Daily
 
         DIRCODES => [qw( NB SB WB EB IN OU GO RT CW CC D1 D2 UP DN A B )],
-
-        #        DAYS_FROM_TRANSITINFO => { reverse
-        #                 qw(
-        #              1234567H DA
-        #              12345    WD
-        #              6        SA
-        #              7H       SU
-        #              67H      WE
-        #              24       TT
-        #              25       TF
-        #              35       WF
-        #              135      MZ
-        #              )
-        #        },
-        
-        LOOP_DIRECTIONS => [ qw( CW CC A B ) ],
+        LOOP_DIRECTIONS     => [qw( CW CC A B )],
+        TRANSBAY_NOLOCALS   => [qw/FS L NX NX1 NX2 NX3 U W/],
+        LINES_TO_BE_SKIPPED => [399],
 
         TRANSITINFO_DAYS_OF => {
             qw(
@@ -90,19 +71,16 @@ BEGIN {
               )
         },
 
-        TRANSBAY_NOLOCALS   => [qw/FS L NX NX1 NX2 NX3 U W/],
-        LINES_TO_BE_SKIPPED => [399],
-
         SIDE_OF => {
             ( map { $_ => 'E' } ( 0 .. 13, qw/15 16 17 20 21 23 98 99/ ) ),
             ( map { $_ => 'W' } (qw/14 18 19 22 24 25 26 97/) ),
         },
 
     );
-    
-     $constants{IS_A_LOOP_DIRECTION}{$_} = 1
+
+    $constants{IS_A_LOOP_DIRECTION}{$_} = 1
       foreach @{ $constants{LOOP_DIRECTIONS} };
-      
+
     $constants{LINE_SHOULD_BE_SKIPPED}{$_} = 1
       foreach @{ $constants{LINES_TO_BE_SKIPPED} };
 
@@ -115,24 +93,33 @@ BEGIN {
     no warnings 'once';
     no strict 'refs';
 
-    while ( ( $name, $value ) = each(%constants) ) {
-        *{$name} = $value;    # supports <sigil>__PACKAGE__::<variable>
+    while ( my ( $name, $value ) = each(%constants) ) {
+        #*{$name} = $value;    # supports <sigil>__PACKAGE__::<variable>
+        my $qualname = __PACKAGE__ . q{::} . $name;
+        my $reftype  = ref($value);
+        if ( $reftype eq 'HASH' ) {
+            const %{$qualname}, %{$value};
+        }
+        elsif ( $reftype eq 'ARRAY' ) {
+            const @{$qualname}, @{$value};
+        }
+        elsif ( $reftype eq 'SCALAR' ) {
+            const ${$qualname}, ${$value};
+        }
+        else {
+            const ${$qualname}, $value;
+        }
     }
 
 } ## tidy end: BEGIN
 
 sub import {
     my $caller = caller;
-
     no strict 'refs';
-
-    # constants
-    while ( ( $name, $value ) = each(%constants) ) {
+    while ( my ( $name, $value ) = each(%constants) ) {
         *{ $caller . q{::} . $name } = $value;
     }
-
     return;
-
 }
 
 1;
@@ -154,43 +141,48 @@ This documentation refers to Actium::Constants version 0.001
    
 =head1 DESCRIPTION
 
-This module exports a series of constant values used by various Actium modules.
-They will be exported by default and are also available using the 
-fully-qualified form, e.g., $Actium::Constants::CRLF .
+This module exports a series of constant values used by various
+Actium modules.  They will be exported by default and are also
+available using the fully-qualified form, e.g., 
+$Actium::Constants::CRLF .
 
 =head1 CONSTANTS
 
-See the code for the values of the constants. Most are obvious. Some exceptions:
+See the code for the values of the constants. Most are obvious.
+Some exceptions:
 
 =over
 
 =item $KEY_SEPARATOR
 
-This contains the C<^]> character (ASCII 29, "Group Separator"), which is 
-used by FileMaker to separate entries in repeating fields. It is also used by
-various Actium routines to separate values, e.g., the Hastus Standard AVL 
-routines use it in hash keys when two or more values are needed to uniquely 
-identify a record. (This is the same basic idea as that intended by perl's 
-C<$;> variable [see L<perlvar/$;>].)
+This contains the C<^]> character (ASCII 29, "Group Separator"),
+which is used by FileMaker to separate entries in repeating fields.
+It is also used by various Actium routines to separate values, e.g.,
+the Hastus Standard AVL routines use it in hash keys when two or
+more values are needed to uniquely identify a record. (This is the
+same basic idea as that intended by perl's C<$;> variable [see
+L<perlvar/$;>].)
 
 =item %LINES_TO_COMBINE
 
-This contains a hard-wired hash of lines. Each key is a line that should be 
-consolidated with another line on its schedule: for example, 59A should 
-appear with 59, and 72M should appear with 72. It is not possible to simply
-assume that a line should appear with all its subsidiary lines since some lines
-do not fit this pattern (386 and 83 go on 86 while 72R does not go on 72).
+This contains a hard-wired hash of lines. Each key is a line that
+should be consolidated with another line on its schedule: for
+example, 59A should appear with 59, and 72M should appear with 72.
+It is not possible to simply assume that a line should appear with
+all its subsidiary lines since some lines do not fit this pattern
+(386 and 83 go on 86 while 72R does not go on 72).
 
 This is not the right place to store this information; 
 it should be moved to a user-accessible database.
 
 =item @SCHEDULE_DAYS
 
-This is a list of valid schedule day codes. Each set of schedules is either the set 
-of schedules for weekdays (WD), Saturdays (SA), or Sundays (SU). Sometimes these can
-be combined, so we have combinations: weekends (WE), and every day (DA). For completeness we also
-have weekdays and Saturdays (WA) and weekdays and Sundays (WU), although usage is expected to be
-extremely rare.
+This is a list of valid schedule day codes. Each set of schedules
+is either the set of schedules for weekdays (WD), Saturdays (SA),
+or Sundays (SU). Sometimes these can be combined, so we have
+combinations: weekends (WE), and every day (DA). For completeness
+we also have weekdays and Saturdays (WA) and weekdays and Sundays
+(WU), although usage is expected to be extremely rare.
 
 =back
 
@@ -202,10 +194,22 @@ See L</%LINES_TO_COMBINE>.
 
 Aaron Priven <apriven@actransit.org>
 
-=head1 LICENSE AND COPYRIGHT
+=head1 COPYRIGHT & LICENSE
 
-This module is free software; you can redistribute it and/or modify it under 
-the same terms as Perl itself. See L<perlartistic>.
+Copyright 2013
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of either:
+
+=over 4
+
+=item * the GNU General Public License as published by the Free
+Software Foundation; either version 1, or (at your option) any
+later version, or
+
+=item * the Artistic License version 2.0.
+
+=back
 
 This program is distributed in the hope that it will be useful, but WITHOUT 
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
