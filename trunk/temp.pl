7004b77e0a0f8b44b7710f770f1e7b3afa1dddf1
aaronpriven@gmail.com <aaronpriven@gmail.com@0dd53fb8-6e72-cf8d-f5b0-1f9b7bbc9dea>
2013-10-23 00:45:47 +0000

    Started writing read-excel routine in temp.pl (to be moved later).

--- trunk/temp.pl
+++ trunk/temp.pl
@@ -2,10 +2,77 @@
 
 use 5.016;
 use Actium::Preamble;
+use Actium::O::Sked;
+use Actium::O::Sked::Trip;
 
-say jt(qw/a b c/);
+use Spreadsheet::XLSX;
 
-use List::MoreUtils('true');
+const my @used_sheets => qw[intro tpsked stopsked];
+const my @mandatory_intros => qw[id days dir];
 
-say true {$_ == 0 } (1,1,0,0,1);
+my $file = '/Users/apriven/Dev/signups/su12/s/xlsx/P_WB_12345.xlsx';
 
+my $xlsx = Spreadsheet::XLSX->new($file);
+
+_check_sheets($xlsx);
+
+my %intros = _get_intros ($xlsx);
+
+sub _get_intros {
+   my $xlsx = shift;
+   my %intros;
+   my $introsheet = $xlsx->worksheet('intro');
+   my ( $row_min, $row_max ) = $introsheet->row_range();
+   my ( $col_min, $col_max ) = $introsheet->col_range(); 
+   
+   if ($col_max <= $col_min) {
+       croak "Not enough columns in intro sheet of $file";
+   } elsif ($col_max != ($col_min +1) ) {
+       croak "Too many columns in intro sheet of $file";
+   }
+    
+   
+   
+   
+   
+   
+   for my $row ($row_min .. $row_max) {
+       my $attribute = $introsheet->get_cell( $row, $col_min)->value;
+       my $value = $introsheet->get_cell( $row, $col_min+1)->value;
+       if (defined($attribute) xor defined($value) ) {
+           # if only one is defined,
+           
+           # check all this
+        
+        
+       }
+       $intros{$attribute} = $value;
+   }
+   
+   return %intros;
+ 
+}
+
+sub _check_sheets {
+    my $xlsx = shift;
+
+    my @names = sort map { $_->get_name } $xlsx->worksheets;
+
+    my %is_a_sheet;
+    $is_a_sheet{$_} = 1 foreach @names;
+
+    foreach my $sheetname (@used_sheets) {
+        unless ( $is_a_sheet{$sheetname} ) {
+            croak "Can't locate sheet in $file: $sheetname";
+        }
+    }
+
+    delete @is_a_sheet{@used_sheets};
+
+    foreach my $sheetname ( keys %is_a_sheet ) {
+        carp "Unrecognized sheet: $sheetname";
+    }
+    
+    return;
+
+}
