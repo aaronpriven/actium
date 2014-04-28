2637e1a61fc45233c70d928b22a93e6b56ec954b
aaronpriven <aaronpriven@0dd53fb8-6e72-cf8d-f5b0-1f9b7bbc9dea>
2014-04-28 19:01:52 +0000

    Fixes to Flags and PrepareFlags for changes to Stops_Neue table.

--- trunk/Actium/Flags.pm
+++ trunk/Actium/Flags.pm
@@ -11,50 +11,78 @@ package Actium::Flags 0.004;
 use Actium::Preamble;
 use Actium::Term;
 
-const my @COLUMNS =>
-  qw[ 
-  flagtype_filename    flagtype_master_page    h_stp_511_id
-  c_description_full   p_decals  
-  ];
+const my @COLUMNS => qw[
+  u_flagtype_id        flagtype_filename    flagtype_master_page
+  h_stp_511_id         c_description_full   p_decals
+];
+
+const my %COLUMN_INDEX_OF => ( map { $COLUMNS[$_] => $_ } 0 .. $#COLUMNS );
 
 const my $COLUMNS_SQL => join( ', ', @COLUMNS );
+
+const my %SKIPPED_EXPLANATION_OF => (
+    no_flagtype => 'The following had no flagtype set',
+    no_decals   => 'The following had no decals set'
+);
 
 sub flag_assignments {
 
     my $actium_db  = shift;
     my $actium_dbh = $actium_db->dbh;
 
-    my $query = qq[
+    my $query = <<"EOT";
+
     SELECT $COLUMNS_SQL 
     FROM Stops_Neue 
-       LEFT JOIN Flagtypes ON Stops.u_flagtype_id = Flagtypes.flagtype_id
-    WHERE Flags.flag_to_print_next_run = 1
-    ];
+       LEFT JOIN Flagtypes ON Stops_Neue.u_flagtype_id = Flagtypes.flagtype_id
+    WHERE Stops_Neue.u_flag_to_print_next_run = 1
+
+EOT
 
     my $sth = $actium_dbh->prepare($query);
     $sth->execute();
 
-    my %rows_of_file;
+    my ( %rows_of_file, %skipped_because );
 
     while ( my $row_r = $sth->fetchrow_arrayref ) {
-    	my ($file, @rest) = @{$row_r};
-    	$file =~ s/\.indd\z//;
-        # DBI reuses the same hashref over and over, so have to make that
-        # copy each time
-        push @{$rows_of_file{$file}}, \@rest;
-    }
+
+        my ( $flagtype, $file, $master, $stopid, $description, $decals )
+          = @{$row_r};
+
+        unless ($flagtype) {
+            push @{ $skipped_because{no_flagtype} }, $stopid;
+            next;
+        }
+
+        unless ($decals) {
+            push @{ $skipped_because{no_decals} }, $stopid;
+            next;
+        }
+
+        $master ||= 'A';
+
+        $file =~ s/[.]indd\z//sx;
+
+        push @{ $rows_of_file{$file} },
+          [ $master, $stopid, $description, $decals ];
+
+    } ## tidy end: while ( my $row_r = $sth->...)
     $sth->finish();
-    
-    unless (scalar %rows_of_file) {
-        #emit_text "No flags marked as to print in database.";
-        emit_error;
-        die "Error: No flags marked as to print in database.\n";
+
+    foreach my $reason ( keys %skipped_because ) {
+        my @stops = join( $SPACE, @{ $skipped_because{$reason} } );
+        emit_text "$SKIPPED_EXPLANATION_OF{$reason}: @stops";
     }
-    
+
+    unless ( scalar %rows_of_file ) {
+        emit_error { -reason => 'Error: No flags to prepare.' };
+        return;
+    }
+
     my @rows_by_file;
-    foreach my $file (keys %rows_of_file) {
-    	push @rows_by_file, [ FILE => $file ];
-    	push @rows_by_file, @{$rows_of_file{$file}};
+    foreach my $file ( keys %rows_of_file ) {
+        push @rows_by_file, [ FILE => $file ];
+        push @rows_by_file, @{ $rows_of_file{$file} };
     }
 
     return \@rows_by_file;
@@ -65,7 +93,11 @@ sub flag_assignments_tabbed {
     my $actiumdb        = shift;
     my $assignments_aoa = Actium::Flags::flag_assignments($actiumdb);
 
-    my $tabbed = Actium::Util::aoa2tsv( $assignments_aoa );
+    return unless $assignments_aoa;    # if there was an error
+
+    ## no critic (RequireExplicitInclusion)
+    my $tabbed = Actium::Util::aoa2tsv($assignments_aoa);
+    ## use critic
 
     return $tabbed;
 
@@ -75,8 +107,3 @@ sub flag_assignments_tabbed {
 
 __END__
 
-(these are tabs)
-
-% more 19.5x17-RW13 
-53315   Washington Blvd. at Fremont Blvd., Fremont, near side, going west       210-c   215-a
-55969   Washington Blvd. at Fremont Blvd., Fremont, far side, going east        210-a   215-c
