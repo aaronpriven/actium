004cdf210ebc77b69241dbfd549833053e62b722
Aaron Priven <aaron@priven.com>
2014-05-09 00:49:41 +0000

    Tweak colors, add no-backspace mode. Fixed error to Actium::Preamble about std in/out/err encodings.

--- t/Notify/sample001.pl
+++ t/Notify/sample001.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Actium::O::Notify;
 
-my $notifier = Actium::O::Notify::->new();
+my $notifier = Actium::O::Notify::->new(colorize=>1);
 
 my $notification = $notifier->notify("Contract to build house");
 build_house();
@@ -16,9 +16,13 @@ sub build_house {
     sitework();
     shell();
     $build_nf->text( "
-Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 Vestibulum varius libero nec emitus. Mauris eget ipsum eget quam sodales ornare. Suspendisse nec nibh. Duis lobortis mi at augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-");
+"); 
+
+# line feed at the top of text yields blank line, 
+# but this was present in original Term::Emit sample
+
     mechanical();
     finish();
     $build_nf->done;
