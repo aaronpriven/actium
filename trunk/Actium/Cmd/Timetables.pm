05cf3d5f0a8cc8ccf1153dee283e94642ba2e3b5
aaronpriven <aaronpriven@0dd53fb8-6e72-cf8d-f5b0-1f9b7bbc9dea>
2014-09-22 22:09:18 +0000

    Removed vestigial remnants of xml_db in favor of actiumdb

--- trunk/Actium/Cmd/Timetables.pm
+++ trunk/Actium/Cmd/Timetables.pm
@@ -48,9 +48,6 @@ sub START {
     my $pubtt_folder      = $tabulae_folder->subfolder('pubtt');
     my $multipubtt_folder = $tabulae_folder->subfolder('pub-idtags');
 
-    #my $xml_db = $signup->load_xml;
-    my $xml_db = $actiumdb; # testy test test. Can this work?
-
     my $prehistorics_folder = $signup->subfolder('skeds');
 
     chdir( $signup->path );
@@ -58,21 +55,21 @@ sub START {
     # my %front_matter = _get_configuration($signup);
 
     my @skeds
-      = Actium::O::Sked->load_prehistorics( $prehistorics_folder, $xml_db );
+      = Actium::O::Sked->load_prehistorics( $prehistorics_folder, $actiumdb );
 
     my @all_lines = map { $_->lines } @skeds;
     @all_lines = grep { $_ ne 'BSD' and $_ ne 'BSN'} @all_lines;
     @all_lines = uniq sortbyline @all_lines;
     
     my ($pubtt_contents_with_dates_r , $pubtimetables_r) 
-      = Actium::IDTables::get_pubtt_contents_with_dates( $xml_db, \@all_lines );
+      = Actium::IDTables::get_pubtt_contents_with_dates( $actiumdb, \@all_lines );
 
     @skeds = map { $_->[0] }
       sort { $a->[1] cmp $b->[1] }
       map { [ $_, $_->sortable_id() ] } @skeds;
 
     my ( $alltables_r, $tables_of_r )
-      = Actium::IDTables::create_timetable_texts( $xml_db, @skeds );
+      = Actium::IDTables::create_timetable_texts( $actiumdb, @skeds );
 
     Actium::IDTables::output_all_tables( $tabulae_folder, $alltables_r );
 
