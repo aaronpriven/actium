6131bb8825adc1412ab313475abca30b227e3f34
Aaron Priven <aaron@priven.com>
2014-12-09 05:02:06 +0000

    Some additional documentation & comments

--- Actium/Import/CalculateFields.pm
+++ Actium/Import/CalculateFields.pm
@@ -6,7 +6,7 @@
 
 # Subversion: $Id$
 
-package Actium::Import::CalculateFields 0.007;
+package Actium::Import::CalculateFields 0.008;
 
 use Actium::Preamble;
 use Text::Trim;
@@ -24,9 +24,12 @@ sub hastus_places_import {
     my @place_records = @{ +shift };
 
     my @returned_headers = (
-        ( map { $ORIG_PREFIX . $_ } @place_headers) ,
+        ( map { $ORIG_PREFIX . $_ } @place_headers ),
         map { $CALC_PREFIX . $_ } (qw (city abbrev9 description ))
     );
+    
+    # adds the "h_" prefix to the original headers and the "i_" prefix
+    # to the ones it creates
 
     my @returned_records;
     my $count     = 0;
@@ -48,19 +51,19 @@ sub hastus_places_import {
 
         my $description = titlecase( $field{plc_description} );
 
-        my @new_record
-          = ( @field{@place_headers}, $city, $abbrev9, $description );
+        my @new_record =
+          ( @field{@place_headers}, $city, $abbrev9, $description );
         trim(@new_record);
         push @returned_records, \@new_record;
 
-    } ## tidy end: foreach my $place_record (@place_records)
+    }    ## tidy end: foreach my $place_record (@place_records)
 
     emit_over '100%';
     emit_done;
 
     return \@returned_headers, \@returned_records;
 
-} ## tidy end: sub hastus_places_import
+}    ## tidy end: sub hastus_places_import
 
 sub _abbrev9 {
     my $number     = shift;
@@ -85,9 +88,11 @@ sub hastus_stops_import {
     my @stop_records = @{ +shift };
 
     my @returned_headers = (
-        (map { $ORIG_PREFIX . $_ } @stop_headers),
-        (map { $CALC_PREFIX . $_ } 
-           (qw[on at street_num comment city direction])),
+        ( map { $ORIG_PREFIX . $_ } @stop_headers ),
+        (
+            map { $CALC_PREFIX . $_ }
+              (qw[on at street_num comment city direction])
+        ),
     );
 
     my @returned_records;
@@ -105,12 +110,13 @@ sub hastus_stops_import {
         my %field;
         @field{@stop_headers} = @{$stop_record};
 
-        my ( $on, $at, $stnum, $comment )
-          = _stops_description( $field{stp_description} );
+        my ( $on, $at, $stnum, $comment ) =
+          _stops_description( $field{stp_description} );
 
         _street_name( $on, $at );
 
         my $direction = _direction( $field{stp_corner}, $field{stop_site} );
+
         # stop_site is correct, unlike all other stp_ fields
 
         my $city = $HASTUS_CITY_OF{ $field{stp_district} };
@@ -122,7 +128,7 @@ sub hastus_stops_import {
         trim(@new_record);
         push @returned_records, \@new_record;
 
-    } ## tidy end: foreach my $stop_record (@stop_records)
+    }    ## tidy end: foreach my $stop_record (@stop_records)
 
     emit_over '100%';
 
@@ -130,7 +136,7 @@ sub hastus_stops_import {
 
     return \@returned_headers, \@returned_records;
 
-} ## tidy end: sub hastus_stops_import
+}    ## tidy end: sub hastus_stops_import
 
 const my %ENDING_OF => (
     Av   => 'Ave.',
@@ -156,7 +162,7 @@ const my %ENDING_OF => (
     Sch  => 'School',
     Sq   => 'Square',
     Ter  => 'Terr.',
-    Terr => 'Terr',
+    Terr => 'Terr.',
     Apts => 'Apartments',
 );
 
@@ -183,7 +189,7 @@ sub _street_name {
         }
 
         s/Amtrak Station/Amtrak/i;
-        s/Caltrain Station/Amtrak/i;
+        s/Caltrain Station/Caltrain/i;
         s/N Berkeley BART/North Berkeley BART/i;
         s/BART Station/BART/i;
         s/Bart/BART/i;
@@ -191,58 +197,77 @@ sub _street_name {
         s/\AMlk Jr\z/Martin Luther King Jr. Way/i;
         s/\AMlk Way\z/Martin Luther King Jr. Way/i;
         s/\AFwy /Highway /i;
-        s/\b(Mc) ([A-Z])/$1$2/;
-        s/\bAlvarado Niles\b/Alvarado-Niles/;
-        s/P\s*\&\s*R/Park and Ride/;
+        s/\b(Mc) ([A-Z])/$1$2/i;
+        s/\bAlvarado Niles\b/Alvarado-Niles/i;
+        s/P\s*\&\s*R/Park and Ride/i;
+        # changes “P&R” or “P & R” to “Park and Ride”
         s/Bayfair BART/Bay Fair BART/i;
         s/\ADel Norte BART/El Cerrito del Norte BART/i;
         s/\bJr\b/Jr./i;
-        s/(I-[0-9]{1,3}) Fwy/$1/;
-        s/MacDonald/Macdonald/;
+        s/(I-[0-9]{1,3}) Fwy/$1/i;
+        s/MacDonald/Macdonald/i;
         s/Northport Lp\b/Northport Loop/i;
 
-    } ## tidy end: foreach (@_)
+    }    ## tidy end: foreach (@_)
 
     return;
 
-} ## tidy end: sub _street_name
+}    ## tidy end: sub _street_name
 
 sub _stops_description {
     my $desc = shift;
 
-    my ( $on, $at, $stnum, $comment )
-      = ( $EMPTY_STR, $EMPTY_STR, $EMPTY_STR, $EMPTY_STR );
+    my ( $on, $at, $stnum, $comment ) =
+      ( $EMPTY_STR, $EMPTY_STR, $EMPTY_STR, $EMPTY_STR );
+      # assign these the empty streets to avoid 
+      # "Undefined value in comparison errors
 
     my $rest;
     ( $on, $rest ) = split( /:/, $desc, 2 );
+    # on is everything before colon, if there's a colon
+    # the rest is everything else
 
     if ( $on =~ / at / and not $rest ) {
         ( $on, $rest ) = split( / at /i, $desc, 2 );
     }
+    # or if there's not a colon, then on is everything before " at ",
+    # and $rest is everything else
 
     if ( $on =~ /\#/ and not $rest ) {
         ( $on, $rest ) = split( /\#/i, $desc, 2 );
         $rest = '#' . $rest;
     }
+    # if there's no colon or " at ", but there is a number sign, 
+    # add a number sign to
+    # the rest, so the following code recognizes it as a street number
 
     if ($rest) {
         $rest =~ s{  \( ( .* ) \) } {}sx;
         $comment = $1;
+        # remove the comment from $rest and put it in $comment
 
         if ( $rest =~ /Berkley/ and $comment and $comment =~ /20th/ ) {
             $rest .= " ($comment)";
             $comment = $EMPTY_STR;
         }    # Thomas L. Berkley Way (20th St.) is "at"...
+        
+        # But put it back if it's Thomas L. Berkley Way (20th St.)
 
         if ( $rest =~ /\A\s*#/s or $rest =~ /\A[0-9]+\z/s ) {
             $stnum = $rest;
             $stnum =~ s/\A#//s;
         }
+        # if the rest has a number sign (possibly with spaces), or 
+        # it consists entirely of numbers, put the rest into the street
+        # number
         else {
             $at = $rest;
         }
+        # otherwise put it in "at"
 
         $at =~ s/\bat //i;
+        # remove any literal word "at" (followed by a word break, and including
+        # a subsequent space, and 
     }
 
     if ($comment) {
@@ -255,7 +280,7 @@ sub _stops_description {
 
     return trim( $on, $at, $stnum, $comment );
 
-} ## tidy end: sub _stops_description
+}    ## tidy end: sub _stops_description
 
 const my %DIRECTION_OF => (
     NEFS => "N",
@@ -295,26 +320,61 @@ Hastus exports
 
 Builds the following fields:
 
-==> place_pc.txt <==
+=head2 places
 
-i_city	
+=head3 i_city	
 
 Takes the numeric district code and returns the city.
 
-i_abbrev9	
+=head3 i_abbrev9	
 
 Takes the eight-character place abbreviation (confusingly called
 plc_number in the Hastus export) and adds a space in the middle
 to make it nine characters. This is primarily for compatibility
-with old actium programs and routines that use this.
-Ideally it should be phased out and the plc_identifier used instead.
+with old Actium programs and routines that use this.
+Ideally it should be phased out and the plc_identifier used instead, since
+Actium doesn't use this for display anywhere.
 
-i_description
+=head4 i_description
 
-A titlecased version (using Lingua::EN::Titlecase::Simple) of
+A titlecased version (using the Lingua::EN::Titlecase::Simple) of
 the provided description.
 
-==> stop_pc.txt <==
-stp_511_id	stp_pc_on	stp_pc_at	stp_pc_stnum	stp_pc_comment	stp_pc_city	stp_pc_direction
+=head2 stops
+
+The i_on, i_at, i_comment, and i_street_num fields are the result of parsing 
+the stp_description field from Hastus.
+
+=head3 i_on
+
+This is the street the stop is on, and is the only one that should never be 
+blank. It generally consists of the portion of the description before the 
+colon.
+
+=head3 i_at
+
+This is the cross-street of the stop. It can be blank, and often is. 
+
+=head3 i_comment
+
+If there is a portion of the description in parentheses, that is extracted 
+into i_comment.
+
+=head3 i_street_num
+
+If there is a portion of the description after a number sign, that is extracted
+into i_street_num.
+
+=head3 i_city
+
+Takes the numeric district code and returns the name for that district. (Note
+that some districts are counties or other areas.)
+
+=head3 i_direction
+
+Combines the corner (northwest corner, southwest corner, etc.) 
+and site (nearside / farside) information and deduces the direction of travel
+from it.
+
 
 
