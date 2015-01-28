47a11907370ec8c9039e13d4feb94068ce262eaf
aaronpriven <aaronpriven@0dd53fb8-6e72-cf8d-f5b0-1f9b7bbc9dea>
2015-01-28 23:39:16 +0000

    First cut at Cmd::ZipCodes and at Cmd::Config::GeonamesAuth

---
+++ trunk/Actium/Cmd/Config/GeonamesAuth.pm
@@ -0,0 +1,43 @@
+# Actium/Cmd/Config/GeonamesAuth.pm
+
+# Configuration and command-line options for Geonames authentification
+
+# Subversion: $Id$
+
+package Actium::Cmd::Config::GeonamesAuth 0.009;
+
+use Actium::Preamble;
+use Actium::Options qw(option add_option);
+use Actium::Term;
+use Actium::O::Photos::Flickr::Auth;
+
+use Sub::Exporter -setup => { exports => [qw(geonames_username)] };
+
+my %description_of_option = ( username => 'Geonames API username', );
+
+foreach ( keys %description_of_option ) {
+    add_option "geonames_$_=s", $description_of_option{$_};
+}
+
+const my $CONFIG_SECTION => 'Geonames';
+
+sub geonames_username {
+
+    my $config_obj = shift;
+    my %config     = $config_obj->section($CONFIG_SECTION);
+
+    my %params;
+    foreach ( keys %description_of_option ) {
+
+        my $optname = "geonames_$_";
+        $params{$_} = option($optname) // $config{$_}
+          // Actium::Term::term_readline( $description_of_option{$_} . ':' );
+    }
+
+    return $params{username};
+
+}
+
+1;
+
+__END__
