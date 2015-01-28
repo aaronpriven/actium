5392af59a63b82ab232db89746355ba1629118e2
Aaron Priven <aaron@priven.com>
2015-01-28 00:23:34 +0000

    Added folded_in to Actium::Util. Moved more things to DecalPreparation from Cmd::Decalcount, and changed read_decal_list to use 2DArray.

--- Actium/Cmd/DecalCount.pm
+++ Actium/Cmd/DecalCount.pm
@@ -9,8 +9,7 @@ package Actium::Cmd::DecalCount 0.008;
 use Actium::Preamble;
 use Actium::Util('add_before_extension');
 use Actium::Cmd::Config::ActiumFM ('actiumdb');
-use Actium::DecalPreparation(
-    qw/write_decalcount_xlsx count_decals decals_of_stop read_decal_list/);
+use Actium::DecalPreparation( qw/make_decal_count /);
 
 sub HELP {
     say 'Makes spreadsheet to calculate decal count.';
@@ -26,22 +25,8 @@ sub START {
 
     my $input_file = shift @{ $params{argv} };
     my $output_file = add_before_extension( $input_file, 'counted' );
-
-    my %lines_of = %{ read_decal_list($input_file) };
-
-    my $db_decals_of_r = $actium_db->all_in_column_key(qw/Stops_Neue p_decals/);
-
-    my ( $decals_of_r, $found_decals_of_r ) =
-      decals_of_stop( \%lines_of, $db_decals_of_r );
-
-    my $count_of_r = count_decals($found_decals_of_r);
-
-    write_decalcount_xlsx(
-        output_file     => $output_file,
-        decals_of       => $decals_of_r,
-        found_decals_of => $found_decals_of_r,
-        count_of        => $count_of_r
-    );
+    
+    make_decal_count ($input_file, $output_file, $actium_db);
 
     return;
 
