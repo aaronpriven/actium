b48b8750d93ea4ec234523da3b81eb07d53a67b0
aaronpriven <aaronpriven@0dd53fb8-6e72-cf8d-f5b0-1f9b7bbc9dea>
2015-03-10 21:57:53 +0000

    Added program to make text from the timetable matrix document. Probably this should be in the database, but it's not yet.

---
+++ trunk/Actium/Cmd/TimetableMatrixText.pm
@@ -0,0 +1,103 @@
+# Actium/Cmd/TimetableMatrixText.pm
+
+# Reads the timetable matrix and produces text for it.
+
+# Subversion: $Id$
+
+package Actium::Cmd::TimetableMatrixText 0.009;
+
+use warnings;
+use 5.016;
+
+use Actium::Preamble;
+use Actium::Util('joinseries_ampersand');
+use Actium::O::Folder;
+
+sub HELP {
+
+    say <<'HELP' or die q{Can't write to STDOUT};
+Reads a timetable matrix file specified on the command line and outputs
+the appropriate text.
+HELP
+
+    Actium::Term::output_usage();
+
+    return;
+}
+
+sub START {
+    my $class = shift;
+
+    my %params = @_;
+
+    my @argv = @{ $params{argv} };
+
+    my $filespec = shift @argv;
+    die "No input file given" unless $filespec;
+
+    my ( $folder, $filename ) = Actium::O::Folder->new_from_file($filespec);
+
+    my $sheet = $folder->load_sheet($filename);
+
+    $sheet->prune_space;
+
+    my @headers = $sheet->shift_row;
+    s#\s+#/#g foreach @headers;
+    s#/+#/#g  foreach @headers;
+
+    my @columns_to_use = $sheet->shift_row;
+
+    my %timetables_of;
+    my %notready_timetables_of;
+    my %each_of;
+
+    foreach my $row_r ( @{$sheet} ) {
+        my @entries = @{$row_r};
+        my $center  = $entries[0];
+        next unless $center;
+
+        my $each = $entries[1];
+        next unless $each;
+
+        my $done = $entries[-1];
+        next if $done;
+
+        my @timetables;
+        my $notready;
+
+        foreach my $idx ( 2 .. $#entries ) {
+            next unless $entries[$idx] eq 'X';
+            $notready = 1 if $columns_to_use[$idx] ne 'X';
+            next unless $headers[$idx] ne '?';
+            push @timetables, $headers[$idx];
+        }
+
+        unless ($notready) {
+            $timetables_of{$center} = \@timetables;
+        }
+        else {
+            $notready_timetables_of{$center} = \@timetables;
+        }
+        $each_of{$center} = $each;
+
+    }
+
+    foreach my $center ( ( sort keys %timetables_of ),
+        ( sort keys %notready_timetables_of ) )
+    {
+        my @timetables =
+          @{ $timetables_of{$center} || $notready_timetables_of{$center} };
+        my $each  = $each_of{$center};
+        my $total = $each * scalar @timetables;
+        next unless scalar @timetables;
+
+        say "$center. Weight: _________________________";
+        print "$each of these timetables: ";
+        @timetables = 'None' unless @timetables;
+        print joinseries_ampersand (@timetables), " (total: $total)\n\n";
+
+    }
+
+}    ## tidy end: sub START
+
+1;
