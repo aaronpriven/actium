8cf0af5515aa9f37b8b3521406bc06ee0dd9c0af
Aaron Priven <aaron@priven.com>
2015-07-15 14:12:37 -0700

    Modified LinesByCity to be a Cmd module, and various perlcritic nits fixed in that and StorableAVL and TabSkeds

--- Actium/Cmd/LinesByCity.pm
+++ Actium/Cmd/LinesByCity.pm
@@ -1,26 +1,11 @@
-#!/ActivePerl/bin/perl
+#/Actium/Cmd/LinesByCity.pm
 
-use strict;
+package Actium::Cmd::LinesByCity 0.010;
 
-# initialization
-
-our $VERSION = 0.010;
-
-use FindBin('$Bin');  ### DEP ###
-   # so $Bin is the location of the very file we're in now
-
-use lib ($Bin);  ### DEP ###
-   # there are few enough files that it makes sense to keep
-   # main program and library in the same directory
-
+use Actium::Preamble;
 use Actium::Files::FileMaker_ODBC (qw[load_tables]);
-
-use Actium::Sorting::Line (qw(sortbyline));
-
-use Actium::Options (qw<option add_option init_options>);
+use Actium::Sorting::Line         (qw(sortbyline));
 use Actium::O::Folders::Signup;
-
-init_options();
 
 #my $signupdir = Actium::O::Folders::Signup->new();
 #chdir $signupdir->path();
@@ -30,56 +15,65 @@ init_options();
 
 # read in FileMaker Pro data into variables in package main
 
-our ( @stops);
+sub HELP { return say 'Help not implemented.' }
 
-load_tables(
-    requests => {
-        Stops_Neue => {
-            array => \@stops,
-            fields => [qw[
-            h_stp_511_id p_active p_lines c_city
-            ]],
-        },
+sub START {
+
+    my (@stops);
+
+    load_tables(
+        requests => {
+            Stops_Neue => {
+                array  => \@stops,
+                fields => [
+                    qw[
+                      h_stp_511_id p_active p_lines c_city
+                      ]
+                ],
+            },
+        }
+    );
+
+    my %lines_of;
+    my %cities_of;
+
+    foreach my $stop (@stops) {
+
+        next unless $stop->{p_active};
+
+        my @routes = split( $SPACE, $stop->{p_lines} );
+        foreach (@routes) {
+
+            next if /NULL/s;
+            my $city = $stop->{c_city};
+            $city =~ s/^\s+//s;
+            $city =~ s/\s+$//s;
+            $lines_of{$city}{$_}++;
+            $cities_of{$_}{$city}++;
+        }
     }
-);
 
-my %lines_of;
-my %cities_of;
+    my @cities = sort keys %lines_of;
 
-foreach my $stop (@stops) {
+    my @lines = sortbyline keys %cities_of;
 
-   next unless $stop->{p_active};
+    foreach my $line (@lines) {
+        print "\t$line";
+    }
+    print "\n";
 
-   my @routes = split(' ' , $stop->{p_lines});
-   foreach (@routes) {
+    foreach my $city (@cities) {
+        print "$city:";
+        foreach my $line (@lines) {
+            my $x = $lines_of{$city}{$line} ? 'X' : $EMPTY_STR;
+            print "\t$x";
+        }
 
-      next if /NULL/;
-      my $city = $stop->{c_city};
-      $city =~ s/^\s+//;
-      $city =~ s/\s+$//;
-      $lines_of{$city}{$_}++;
-      $cities_of{$_}{$city}++;
-   }
-}
+        print "\n";
 
-my @cities = sort keys %lines_of;
+    }
 
-my @lines = sortbyline keys %cities_of;
+    return;
+} ## tidy end: sub START
 
-foreach my $line (@lines) {
-   print "\t$line";
-}
-print "\n";
-
-foreach my $city (@cities) {
-   print "$city:";
-   foreach my $line (@lines) {
-      my $x = $lines_of{$city}{$line} ? 'X' : '';
-      print "\t$x";
-   }
-
-   print "\n";
-
-}
-
-
+1;
