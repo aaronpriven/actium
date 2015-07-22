c7714bede7195f6901e71fecc9482331e4905ac2
Aaron Priven <aaron@priven.com>
2015-07-22 13:02:12 -0700

    Added Actium::AllUtils
    Added the Actium::AllUtils module, which loads a whole bunch of
    modules but (unlike Actium::Preamble) doesn't export them to the
    caller's namespace. Instead only loads them to its own namespace,
    and makes an alias "u" to that namespace. So utility subroutines
    can be called as "u::subname" which is easier than
    Actium::Util::subname or whatever it was originally from, yet
    still continues to give some indication that it's from another
    package and not from here.
    
    An experiment, at least for now.
    
    (Is "u" the right package? Maybe package "_"?)

---
+++ Actium/AllUtils.pm
@@ -0,0 +1,36 @@
+package Actium::AllUtils 0.010;
+
+# probably to move to Actium::Preamble eventually
+
+use 5.022;
+use warnings;
+
+BEGIN {
+    # make the 'u' package an alias to the Actium::AllUtils package
+    no strict 'refs';
+    *u:: = \*Actium::AllUtils::;
+}
+
+use Actium::Constants;
+use Actium::Crier(':all');
+use Actium::Sorting::Line(qw/byline sortbyline/);
+use Actium::Util(':all');
+use Carp qw(cluck longmess shortmess);    ### DEP ###
+use Const::Fast;                          ### DEP ###
+use HTML::Entities (qw[encode_entities decode_entities]);    ### DEP ###
+use List::AllUtils(':all');                                  ### DEP ###
+use Module::Runtime ('require_module');                      ### DEP ###
+use Params::Validate;                                        ### DEP ###
+use POSIX (qw/ceil floor/);                                  ### DEP ###
+use Text::Trim;                                              ### DEP ###
+
+BEGIN {
+    # modules with no 'all' tag
+    require Scalar::Util;
+    Scalar::Util::->import(@Scalar::Util::EXPORT_OK);
+    require Hash::Util;
+    Hash::Util::->import(@Hash::Util::EXPORT_OK);
+}
+
+1;
+
