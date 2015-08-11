0525afe54d6e88d8cd5f2d50c86157270839d64e
Aaron Priven <aaron@priven.com>
2015-08-11 13:36:40 -0700

    Modfications and docs for TimetableMatrixText

--- lib/Actium/Cmd/TimetableMatrixText.pm
+++ lib/Actium/Cmd/TimetableMatrixText.pm
@@ -8,9 +8,9 @@ use warnings;
 use 5.016;
 
 use Actium::Preamble;
-use Actium::Util('joinseries_ampersand');
 use Actium::O::Folder;
 use Actium::Sorting::Line('sortbyline');
+use Text::Trim ('trim');
 
 sub HELP {
 
@@ -22,33 +22,36 @@ HELP
     return;
 }
 
+my %factor_of;
+
 sub START {
-    my ($class, $env) = @_;
+    my ( $class, $env ) = @_;
     my @argv = $env->argv;
 
     my $filespec = shift @argv;
     die "No input file given" unless $filespec;
 
-    my ( $folder, $filename ) = Actium::O::Folder->new_from_file($filespec);
+    my ( $folder,   $filename ) = Actium::O::Folder->new_from_file($filespec);
+    my ( $filepart, $fileext )  = u::file_ext($filespec);
 
     my $sheet = $folder->load_sheet($filename);
 
     $sheet->prune_space;
 
-    my @headers = $sheet->shift_row;
-    s#\s+#/#g foreach @headers;
-    s#/+#/#g  foreach @headers;
+    my @tt_names = $sheet->shift_row;
+    s#\s+#/#g foreach @tt_names;    # spaces to slash
+    s#/+#/#g  foreach @tt_names;    # consecutive slashes to one slash
 
     my @columns_to_use = $sheet->shift_row;
 
     my %timetables_of;
-    my %notready_timetables_of;
     my %each_of;
 
     my %centers_of;
 
     my $type = ' BART';
 
+  ROW:
     foreach my $row_r ( @{$sheet} ) {
         my @entries = @{$row_r};
         my $center  = $entries[0];
@@ -60,71 +63,133 @@ sub START {
             if ( $center =~ /LIBRARIES/ ) {
                 $type = ' Library';
             }
+            elsif ( $center =~ /BART/ ) {
+                $type = ' BART',;
+            }
             else {
                 $type = '';
             }
 
             next;
         }
-        
+
         $center = "$center$type";
 
         #my $done = $entries[-1];
         #next if $done;
 
         my @timetables;
-        my $notready;
+        my $group;
 
         foreach my $idx ( 2 .. $#entries ) {
-            next unless $entries[$idx] eq 'X';
-            $notready = 1 if $columns_to_use[$idx] ne 'X';
-            next unless $headers[$idx] ne '?';
-            push @timetables, $headers[$idx];
+            my $entry = $entries[$idx];
+            next unless $entry;
 
-            push @{ $centers_of{ $headers[$idx] } }, $center;
-        }
+            $entry =~ s/\s+//;    # remove spaces
+            $entry = 1 unless $entry =~ /\A[0-9.]+\z/;
+            next if $entry == 0;
 
-        unless ($notready) {
-            $timetables_of{$center} = \@timetables;
-        }
-        else {
-            $notready_timetables_of{$center} = \@timetables;
-        }
+            my $tt_name = $tt_names[$idx];
+            $factor_of{"$center\0$tt_name"} = $entry;
+
+            my $thisgroup = uc( $columns_to_use[$idx] );
+
+            next if not $thisgroup;
+
+            $group = $thisgroup if ( not $group ) or $group gt $thisgroup;
+
+            push @timetables, $tt_name;
+            push @{ $centers_of{$tt_name} }, $center;
+
+        } ## tidy end: foreach my $idx ( 2 .. $#entries)
+
+        next ROW if not @timetables;
+
+        $timetables_of{$group}{$center} = \@timetables;
+
         $each_of{$center} = $each;
 
-    }
+    } ## tidy end: ROW: foreach my $row_r ( @{$sheet...})
 
-    foreach my $center ( ( sort keys %timetables_of ),
-        ( sort keys %notready_timetables_of ) )
-    {
-        my @timetables =
-          @{ $timetables_of{$center} || $notready_timetables_of{$center} };
-        my $each  = $each_of{$center};
-        my $total = $each * scalar @timetables;
-        next unless scalar @timetables;
+    my $numgroups = scalar keys %timetables_of;
 
-        say "$center. Weight: _________________________";
-        print "$each of these timetables: ";
-        @timetables = 'None' unless @timetables;
-        print joinseries_ampersand (@timetables), " (total: $total)\n\n";
+    my $outfile = "$filepart-centers.txt";
+    my $textfh  = $folder->open_write($outfile);
 
-    }
+    foreach my $group ( sort keys %timetables_of ) {
 
-    say "\n";
+        foreach my $center ( sort keys %{ $timetables_of{$group} } ) {
+
+            my @timetables = @{ $timetables_of{$group}{$center} };
+            my $each       = $each_of{$center};
+            next unless scalar @timetables;
+            my $total = 0;
+
+            my $grouptext = $numgroups > 1 ? " (Group $group)" : $EMPTY;
+
+            my %tts_of_quantity;
+            my %quantity_of;
+            foreach my $tt_name (@timetables) {
+                my $quantity = quantity( $center, $tt_name, $each );
+                $quantity_of{$tt_name} = $quantity;
+                push @{ $tts_of_quantity{$quantity} }, $tt_name;
+                $total += $quantity;
+            }
+
+            say $textfh "$center.$grouptext Weight: _________________________";
+
+            if ( 1 == scalar keys %tts_of_quantity ) {
+                print $textfh "$each of these timetables: ";
+                print $textfh u::joinseries_ampersand(@timetables), " ";
+            }
+            else {
+
+                my @tt_texts = map {"$quantity_of{$_} of $_"} @timetables;
+                print $textfh "These timetables: ";
+
+                my @all_quantities = sort { $a <=> $b } keys %tts_of_quantity;
+                foreach my $quantity (@all_quantities) {
+                    print $textfh "$quantity each of ";
+                    my @thesetts = @{ $tts_of_quantity{$quantity} };
+
+                    print $textfh u::joinseries_ampersand(@thesetts), ". ";
+
+                }
+
+            }
+            print $textfh "(total: $total)\n\n";
+
+        } ## tidy end: foreach my $center ( sort keys...)
+
+    } ## tidy end: foreach my $group ( sort keys...)
+    close $textfh or die "Can't close $outfile: $!";
+
+    my $ttlistfile = "$filepart-ttlist.txt";
+
+    my $listfh = $folder->open_write($ttlistfile);
 
     foreach my $timetable ( sortbyline keys %centers_of ) {
         my @centers = @{ $centers_of{$timetable} };
-        say "Timetable for $timetable:";
+        say $listfh "Timetable for $timetable:";
 
         foreach my $center (@centers) {
-            unless ($each_of{$center} <100) {
-            say "$center: $each_of{$center}";
-            }
-        }
-        say "\n";
 
+            my $quantity = quantity( $center, $timetable, $each_of{$center} );
+            say $listfh "$center: $quantity";
+        }
+        print $listfh "\n";
     }
 
-}    ## tidy end: sub START
+} ## tidy end: sub START
+
+sub quantity {
+    my ( $center, $tt_name, $each ) = @_;
+
+    my $factor = $factor_of{"$center\0$tt_name"} // 1;
+    my $quantity = u::ceil( $factor * $each );
+
+    return $quantity;
+
+}
 
 1;
