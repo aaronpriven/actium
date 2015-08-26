bd67b9d19ce17824e8cd4be15954509f083fc560
Aaron Priven <aaron@priven.com>
2015-08-25 22:33:38 -0700

    Flyweight objects, Frequency, find-deps
    find-deps: removed dependency on List::MoreUtils since it is irritating
    to have to satisfy a dependency before running the program that determines
    what dependencies there are.
    
    Cmd::Frequency: more work.
    
    O::Dir, O::Days, O::Time: noted dependency on MooseX::Role::Flyweight.
    
    O::Time: Made a note on the timestr cache and considering whether to
    just have one cache. Also added init_arg => undef to the presentation
    methods (ap, apbx, t24).

--- lib/Actium/O/Dir.pm
+++ lib/Actium/O/Dir.pm
@@ -11,6 +11,7 @@ use Actium::Moose;
 use MooseX::Storage;    ### DEP ###
 with Storage( traits => ['OnlyWhenBuilt'] );
 with 'MooseX::Role::Flyweight';
+# MooseX::Role::Flyweight ### DEP ###
 
 use Actium::Types qw<DirCode>;
 
