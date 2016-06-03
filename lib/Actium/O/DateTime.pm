81b3966f39ade06b6f9832a7bd7ed40aa970defa
Aaron Priven <aaron@priven.com>
2016-06-02 18:09:06 -0700

    Added ymd & changed locale methods to languages

--- lib/Actium/O/DateTime.pm
+++ lib/Actium/O/DateTime.pm
@@ -11,6 +11,13 @@ use warnings;    ### DEP ###
 use Actium::Moose;
 use DateTime;    ### DEP ###
 
+use overload q{""} => '_stringify';
+
+sub _stringify {
+    my $self = shift;
+    return $self->long_en;
+}
+
 around BUILDARGS => sub {
 
     my $orig = shift;
@@ -29,7 +36,7 @@ around BUILDARGS => sub {
         $args_r = $self->$orig(@_);
     }
 
-    my @args = (qw[datetime strp cldr]);
+    my @args = (qw[datetime strp cldr ymd]);
 
     my $argcount = 0;
     foreach my $arg (@args) {
@@ -66,6 +73,22 @@ around BUILDARGS => sub {
         delete @{$args_r}{qw[cldr pattern]};
 
     }
+    elsif ( exists $args_r->{ymd} ) {
+
+        if ( u::reftype( $args_r->{ymd} ) ne 'ARRAY'
+            or @{ $args_r->{ymd} } != 3 )
+        {
+            croak 'Argument to ymd must be a reference '
+              . 'to a three-element array (year, month, and day) in '
+              . __PACKAGE__ . '->new';
+        }
+
+        my ( $year, $month, $day ) = @{ $args_r->{ymd} };
+        $args_r->{datetime}
+          = DateTime::->new( year => $year, month => $month, day => $day );
+        delete $args_r->{ymd};
+
+    }
 
     return $args_r;
 
@@ -92,16 +115,20 @@ has datetime_obj => (
 #######################################
 ## Return international date formats
 
-my @locales = qw/en_US es_US zh_Hans/;
-my @formats = qw/long full/;
+my %locale_of_language = ( en => 'en_US', es => 'es_US', zh => 'zh_Hans' );
+my @languages = qw/en es zh/; # for order
+# currently happens to be in alpha order, but Vietnamese or Korean
+# would come after Chinese
 
+my @formats = qw/long full/;
 
 foreach my $format (@formats) {
 
-# This creates methods long_en_US, long_es_US, long_zh_Hans, full_en_US, etc.
+    # This creates methods long_en, long_es, long_zh, full_en, etc.
 
-    foreach my $locale (@locales) {
-        has "${format}_$locale" => (
+    foreach my $language ( keys %locale_of_language ) {
+        my $locale = $locale_of_language{$language};
+        has "${format}_$language" => (
             is      => 'ro',
             isa     => 'Str',
             lazy    => 1,
@@ -127,8 +154,8 @@ foreach my $format (@formats) {
 
         );
 
-    } ## tidy end: foreach my $locale (@locales)
-    
+    } ## tidy end: foreach my $language ( keys...)
+
     # This creates longs and fulls
 
     has "${format}s_r" => (
@@ -138,48 +165,24 @@ foreach my $format (@formats) {
         isa      => 'ArrayRef[Str]',
         traits   => ['Array'],
         handles  => { "${format}s" => 'elements' },
-        builder  => sub {
+        lazy     => 1,
+        default  => sub {
             my $self = shift;
 
             my @return;
 
-            foreach my $locale (@locales) {
-                my $method = "${format}_$locale";
+            foreach my $language ( @languages ) {
+                my $locale = $locale_of_language{$language};
+
+                my $method = "${format}_$language";
                 push @return, $self->$method;
             }
 
-            return @return;
+            return \@return;
         },
     );
 
 } ## tidy end: foreach my $format (@formats)
-
-1;
-
-__END__
-
-
-my $dt = DateTime->new(%date);
-
-
-
-my @locales = qw/en_US es_US zh_Hans/;
-
-foreach my $locale (@locales) {
-   my $dl = DateTime::Locale->load($locale);
-   my $pattern = $dl->date_format_full;
-   my $cldr = DateTime::Format::CLDR->new(
-    locale      => $locale,
-     pattern => $pattern, 
-  );
-  print $cldr->format_datetime($dt) , "\t";
-  $cldr->pattern($dl->date_format_long);
-  say $cldr->format_datetime($dt);
-
-}
-
-
-
 
 1;
 
