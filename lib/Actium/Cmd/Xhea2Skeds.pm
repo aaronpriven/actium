1ffbbc027c155e97317018830e343bdfbfb5bb45
Aaron Priven <aaron@priven.com>
2016-07-31 21:34:55 -0700

    Added Cmd::Xhea2Skeds and other files
    Added O::Pattern::Stop.
    
    Moved old unused photo processing to mathoms.

---
+++ lib/Actium/Cmd/Xhea2Skeds.pm
@@ -0,0 +1,28 @@
+package Actium::Cmd::Xhea2Skeds 0.012;
+
+# Takes tab files that are result of XheaImport, and makes schedules
+
+use Actium::Preamble;
+use Actium::Files::Xhea::ToSkeds;
+
+sub OPTIONS {
+    return 'signup';
+}
+
+sub START {
+
+    my ( $class, $env ) = @_;
+    my $signup = $env->signup;
+
+    my $xhea_tab_folder = $signup->subfolder( 'xhea', 'tab' );
+    my $xhea_skeds_folder = $signup->subfolder('xheaskeds');
+
+    Actium::Files::Xhea::ToSkeds::xheatab2skeds(
+        skeds_folder    => $xhea_skeds_folder,
+        xhea_tab_folder => $xhea_tab_folder,
+    );
+
+}
+
+1;
+
