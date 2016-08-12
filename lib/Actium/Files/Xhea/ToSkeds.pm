13fabb5dd21f9f5816ded0069546407b75d107ea
Aaron Priven <aaron@priven.com>
2016-08-11 22:02:44 -0700

    Comment out json and xlsx exports

--- lib/Actium/Files/Xhea/ToSkeds.pm
+++ lib/Actium/Files/Xhea/ToSkeds.pm
@@ -566,19 +566,19 @@ sub _output_skeds {
     my $skeds_r      = shift;
     my $skeds_folder = $signup->subfolder('s');
 
-    my $objfolder = $skeds_folder->subfolder('json_obj');
-    $objfolder->write_files_with_method(
-        OBJECTS   => $skeds_r,
-        METHOD    => 'json',
-        EXTENSION => 'json',
-    );
-
-    my $xlsxfolder = $skeds_folder->subfolder('xlsx');
-    $xlsxfolder->write_files_with_method(
-        OBJECTS   => $skeds_r,
-        METHOD    => 'xlsx',
-        EXTENSION => 'xlsx',
-    );
+#    my $objfolder = $skeds_folder->subfolder('json_obj');
+#    $objfolder->write_files_with_method(
+#        OBJECTS   => $skeds_r,
+#        METHOD    => 'json',
+#        EXTENSION => 'json',
+#    );
+#
+#    my $xlsxfolder = $skeds_folder->subfolder('xlsx');
+#    $xlsxfolder->write_files_with_method(
+#        OBJECTS   => $skeds_r,
+#        METHOD    => 'xlsx',
+#        EXTENSION => 'xlsx',
+#    );
 
     my $spacedfolder = $skeds_folder->subfolder('spaced');
     $spacedfolder->write_files_with_method(
