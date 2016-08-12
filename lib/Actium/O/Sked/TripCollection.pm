41462906ab15e49cbb82c4fbdeb10924f72efeba
Aaron Priven <aaron@priven.com>
2016-08-11 19:16:23 -0700

    Keep trips sorted by stop times in TripCollection

--- lib/Actium/O/Sked/TripCollection.pm
+++ lib/Actium/O/Sked/TripCollection.pm
@@ -10,6 +10,7 @@ has 'trips_r' => (
     isa      => 'ArrayRef[Actium::O::Sked::Trip]',
     required => 1,
     handles  => { trips => 'elements', trip => 'get', trip_count => 'count' },
+    trigger  => &_sort_by_stoptimes,
 );
 
 sub trips_by_day {
@@ -32,8 +33,8 @@ sub trips_by_day {
 
     for my $skedday ( keys %trips_of_skedday ) {
 
-        $tripcollection_of{$skedday} = 
-          $class->new( trips_r => $trips_of_skedday{$skedday} );
+        $tripcollection_of{$skedday}
+          = $class->new( trips_r => $trips_of_skedday{$skedday} );
 
     }
 
@@ -42,7 +43,7 @@ sub trips_by_day {
 } ## tidy end: sub trips_by_day
 
 sub _assemble_skeddays {
-    my $self = shift;
+    my $self           = shift;
     my $trips_of_day_r = shift;
     my @days           = sort keys %{$trips_of_day_r};
     my ( %already_found_day, %trips_of_skedday );
@@ -65,7 +66,8 @@ sub _assemble_skeddays {
             my $inner_trips_r = $trips_of_day_r->{$inner_day};
 
             if ( my $merged_trips_r
-                = $self->_merge_if_appropriate( $found_trips_r, $inner_trips_r ) )
+                = $self->_merge_if_appropriate( $found_trips_r, $inner_trips_r )
+              )
             {
                 push @found_days, $inner_day;
                 $found_trips_r = $merged_trips_r;
@@ -159,9 +161,9 @@ sub _merge_if_appropriate {
 
     my $in_both = ( u::max( $inner_count, $outer_count ) ) - $only_in_either;
 
-    if (    $only_in_either <= $MAXIMUM_DIFFERING_TIMES
-#        and $in_both > ( $MINIMUM_TIMES_MULTIPLIER * $only_in_either ) 
-)
+    if ($only_in_either <= $MAXIMUM_DIFFERING_TIMES
+        #        and $in_both > ( $MINIMUM_TIMES_MULTIPLIER * $only_in_either )
+      )
     {
         my $trips_to_merge_r
           = Actium::O::Sked::Trip->stoptimes_sort( @{$outer_trips_r},
@@ -182,7 +184,7 @@ sub _merge_if_appropriate {
 } ## tidy end: sub _merge_if_appropriate
 
 sub _trips_are_identical {
-    my $self = shift;
+    my $self          = shift;
     my $outer_trips_r = shift;
     my $inner_trips_r = shift;
 
@@ -194,137 +196,88 @@ sub _trips_are_identical {
 
 }
 
+my $common_stop_cr = sub {
+    # returns undef if there's no stop in common, or
+    # the stop to sort by if there is one
 
+    my @trips = @_;
+    my $common_stop;
+    my $last_to_search
+      = ( List::Util::min( map { $_->stoptime_count } @trips ) ) - 1;
 
+  SORTBY_STOP:
+    for my $stop ( 0 .. $last_to_search ) {
+      SORTBY_TRIP:
+        for my $trip (@trips) {
+            next SORTBY_STOP if not defined $trip->stoptime($stop);
+        }
+        $common_stop = $stop;
+        last SORTBY_STOP;
+    }
 
+    return $common_stop;
 
+};
 
-#sub BUILD {
-# my $self = shift;
-#    $self->_sort_by_stoptimes;
-#    $self->_merge_if_same
-#
-#
-#
-#}
-#
-#my $common_stop_cr = sub {
-#
-#    # returns undef if there's no stop in common, or
-#    # the stop to sort by if there is one
-#
-#    my @trips = @_;
-#    my $common_stop;
-#    my $last_to_search
-#      = ( List::Util::min( map { $_->stoptime_count } @trips ) ) - 1;
-#
-#  SORTBY_STOP:
-#    for my $stop ( 0 .. $last_to_search ) {
-#      SORTBY_TRIP:
-#        for my $trip (@trips) {
-#            next SORTBY_STOP if not defined $trip->stoptime($stop);
-#        }
-#        $common_stop = $stop;
-#        last SORTBY_STOP;
-#    }
-#
-#    return $common_stop;
-#
-#};
-#
-#sub _sort_by_stoptimes {
-#    # TODO - generalize to sort by placetimes or stoptimes
-#
-#    my $self = shift;
-#    my @trips = $self->trips;
-#
-#    my $common_stop = $common_stop_cr->(@trips);
-#
-#    if ( defined $common_stop ) {
-#
-#        # sort trips with a common stop
-#
-#        my @cache = map {
-#            [   $_->stoptime($common_stop),    # 0
-#                $_->average_stoptime,          # 1
-#                $_,                            # 2
-#            ]
-#          } @trips;
-#
-#        @cache = sort {
-#                 $a->[0] <=> $b->[0]
-#              or $a->[1] <=> $b->[1]
-#              or $a->[2]->sortable_days cmp $b->[2]->sortable_days
-#          } @cache;
-#
-#        @trips = map { $_->[2] } @cache;
-#
-#
-#        # a schwartzian transform with two criteria --
-#        # either the common stop, or if those times are the same,
-#        # the average.
-#        # if both of those tie, use sortable_days (not put into the
-#        # cache because will be used very very rarely)
-#
-#    } ## tidy end: if ( defined $common_stop)
-#    else {
-#        # sort trips without a common stop for all of them
-#
-#        @trips = sort {
-#
-#            my $common = $common_stop_cr->( $a, $b );
-#
-#            defined $common
-#              ? ( $a->stoptime($common) <=> $b->stoptime($common)
-#                  or $a->average_stoptime <=> $b->average_stoptime
-#                  or $a->sortable_days cmp $b->sortable_days )
-#              : $a->average_stoptime <=> $b->average_stoptime
-#              or $a->sortable_days cmp $b->sortable_days;
-#
-#            # if these two trips have a common stop, sort first
-#            # on those common times, and then by the average.
-#
-#            # if they don't, just sort by the average.
-#
-#        } @trips;
-#
-#    } ## tidy end: else [ if ( defined $common_stop)]
-#
-#    $self->_set_trip_r(\@trips);
-#
-#} ## tidy end: sub stoptimes_sort
-#
-#sub merge_trips_if_same {
-#    my $class  = shift;
-#    my %params = %{ +shift };
-#
-#    my @trips   = @{ $params{trips} };
-#    my @methods = @{ $params{methods_to_compare} };
-#
-#    my @newtrips = shift @trips;
-#
-#  TRIP_TO_MERGE:
-#    while (@trips) {
-#        my $thistrip = shift @trips;
-#        my $prevtrip = $newtrips[-1];
-#
-#        foreach my $this_test (@methods) {
-#            if ( $thistrip->$this_test ne $prevtrip->$this_test ) {
-#                push @newtrips, $thistrip;
-#                next TRIP_TO_MERGE;
-#            }
-#        }
-#        # so now we know they are the same
-#
-#        $newtrips[-1] = $prevtrip->merge_trips($thistrip);
-#
-#    }
-#
-#    return \@newtrips;
-#
-#} ## tidy end: sub merge_trips_if_same
-#
-#__PACKAGE__->meta->make_immutable; ## no critic (RequireExplicitInclusion)
+sub _sort_by_stoptimes {
+
+    my $self  = shift;
+    my @trips = $self->trips;
+
+    my $common_stop = $common_stop_cr->(@trips);
+
+    if ( defined $common_stop ) {
+
+        # sort trips with a common stop
+
+        my @cache = map {
+            [   $_->stoptime($common_stop),    # 0
+                $_->average_stoptime,          # 1
+                $_,                            # 2
+            ]
+        } @trips;
+
+        @cache = sort {
+                 $a->[0] <=> $b->[0]
+              or $a->[1] <=> $b->[1]
+              or $a->[2]->sortable_days cmp $b->[2]->sortable_days
+        } @cache;
+
+        @trips = map { $_->[2] } @cache;
+
+        # a schwartzian transform with two criteria --
+        # either the common stop, or if those times are the same,
+        # the average.
+        # if both of those tie, use sortable_days (not put into the
+        # cache because will be used very very rarely)
+
+    } ## tidy end: if ( defined $common_stop)
+    else {
+        # sort trips without a common stop for all of them
+
+        @trips = sort {
+
+            my $common = $common_stop_cr->( $a, $b );
+
+            defined $common
+              ? ( $a->stoptime($common) <=> $b->stoptime($common)
+                  or $a->average_stoptime <=> $b->average_stoptime
+                  or $a->sortable_days cmp $b->sortable_days )
+              : $a->average_stoptime <=> $b->average_stoptime
+              or $a->sortable_days cmp $b->sortable_days;
+
+            # if these two trips have a common stop, sort first
+            # on those common times, and then by the average.
+
+            # if they don't, just sort by the average.
+
+        } @trips;
+
+    } ## tidy end: else [ if ( defined $common_stop)]
+
+    $self->_set_trip_r( \@trips );
+
+} ## tidy end: sub _sort_by_stoptimes
 
 u::immut;
 
