a5d32e8d84be5837f15e011b3bccb2bcd45bc58f
Aaron Priven <aaron@priven.com>
2016-08-15 22:47:04 -0700

    Change Sked::Collection to be array
    Sked::Collection now is primarily an array of skeds, with some
    cross-references in hashes, rather than primarily a hashref.
    This simplifies construction.
    
    Added sked_ids_of_lg (lg = linegroup) for the new tabxchange
    routine in TabSkeds.

--- lib/Actium/O/Sked/Collection.pm
+++ lib/Actium/O/Sked/Collection.pm
@@ -11,32 +11,80 @@ use Actium::O::Sked;
 around BUILDARGS => sub {
     my $orig  = shift;
     my $class = shift;
-    
-    my %init_arg;
-    
-    if (u::is_plain_hashref($_[-1]) ) {
-        my $init_arg_r = pop @_;
-        %init_arg = %$init_arg_r;
+
+    # if they're all objects, treat them as though they were skeds
+    if ( u::all { u::blessed($_) } @_ ) {
+        return $class->$orig( skeds => \@_ );
     }
-        
-    my %sked_obj_of = map { $_->id, $_ } @_ ;
-    
-    $init_arg{sked_obj_of} = \%sked_obj_of;
-    
+
+    # otherwise, normal
+    return $class->$orig(@_);
+
 };
 
-has 'sked_obj_of' => (
+has 'skeds_r' => (
+    is       => 'bare',
+    isa      => 'ArrayRef[Skedlike]',
+    traits   => ['Array'],
+    required => 1,
+);
+
+has '_sked_obj_by_id_r' => (
     is      => 'bare',
-    isa     => 'Skedlike',
+    isa     => 'HashRef[Skedlike]',
     traits  => ['Hash'],
-    default => sub { {} },
+    builder => '_build_sked_obj_by_id_r',
+    lazy    => 1,
     handles => {
         _set_sked_obj => 'set',
         sked_obj      => 'get',
-        #all_skeds => 'values',
+        _skeds        => 'values',
         #ids => 'keys',
     },
 );
+
+sub _build_sked_obj_by_id_r {
+    my $self  = shift;
+    my @skeds = $self->_skeds;
+    my %sked_obj_by_id;
+    foreach my $sked (@skeds) {
+        my $id = $sked->id;
+        $sked_obj_by_id{$id}->@* = $sked;
+    }
+    return \%sked_obj_by_id;
+}
+
+has '_sked_ids_of_lg' => (
+    is      => 'bare',
+    isa     => 'HashRef[ArrayRef[Str]',
+    traits  => ['Hash'],
+    builder => '_build_sked_ids_of_lg',
+    lazy    => 1,
+    handles => { _sked_ids_of_lg_r => 'get' },
+);
+
+sub _build_sked_ids_of_lg {
+    my $self  = shift;
+    my @skeds = $self->_skeds;
+    my %sked_ids_of_lg;
+    foreach my $sked (@skeds) {
+
+        my $id        = $sked->id;
+        my $linegroup = $sked->linegroup;
+        push $sked_ids_of_lg{$linegroup}->@*, $id;
+
+    }
+
+    return \%sked_ids_of_lg;
+
+}
+
+sub sked_ids_of_lg {
+    my $self      = shift;
+    my $linegroup = shift;
+    my @skedids   = $self->_sked_ids_of_lg($linegroup)->@*;
+    return @skedids;
+}
 
 #sub add_sked {
 #    my $self = shift;
@@ -51,7 +99,7 @@ sub load_json {
 
     my @files = $json_folder->glob_plain_files;
 
-    $class->new(map { Actium::O::Sked::->load($_) } @files);
+    $class->new( map { Actium::O::Sked::->load($_) } @files );
 
 }
 
