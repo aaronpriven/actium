fd61e1a56756b841e78dace690fe7c6cca2b2ea0
Aaron Priven <aaron@priven.com>
2016-08-15 19:36:03 -0700

    Added load from json to O::Sked::Collection
    I don't think I'd actually used Actium::O::Sked::Collection before.
    Now loading all the schedule files will load them into this collection.

--- lib/Actium/O/Sked/Collection.pm
+++ lib/Actium/O/Sked/Collection.pm
@@ -5,8 +5,25 @@
 # legacy status 4
 
 package Actium::O::Sked::Collection 0.010;
-
 use Actium::Moose;
+use Actium::O::Sked;
+
+around BUILDARGS => sub {
+    my $orig  = shift;
+    my $class = shift;
+    
+    my %init_arg;
+    
+    if (u::is_plain_hashref($_[-1]) ) {
+        my $init_arg_r = pop @_;
+        %init_arg = %$init_arg_r;
+    }
+        
+    my %sked_obj_of = map { $_->id, $_ } @_ ;
+    
+    $init_arg{sked_obj_of} = \%sked_obj_of;
+    
+};
 
 has 'sked_obj_of' => (
     is      => 'bare',
@@ -15,16 +32,27 @@ has 'sked_obj_of' => (
     default => sub { {} },
     handles => {
         _set_sked_obj => 'set',
-        sked_obj     => 'get',
+        sked_obj      => 'get',
         #all_skeds => 'values',
         #ids => 'keys',
     },
 );
 
-sub add_sked {
-   my $self = shift;
-   my @objs = shift;
-   $self->_set_sked_obj(map { $_->id, $_} @objs );
+#sub add_sked {
+#    my $self = shift;
+#    my @objs = shift;
+#    $self->_set_sked_obj( map { $_->id, $_ } @objs );
+#}
+
+sub load_json {
+    my $class = shift;
+
+    my $json_folder = shift;
+
+    my @files = $json_folder->glob_plain_files;
+
+    $class->new(map { Actium::O::Sked::->load($_) } @files);
+
 }
 
 u::immut;
