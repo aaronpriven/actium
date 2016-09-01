28d400cc613d9b6218409768f1f8b65a3f6f5c3c
Aaron Priven <aaron@priven.com>
2016-09-01 11:49:12 -0700

    Scratch: make flag clusters

--- lib/Actium/Cmd/Scratch.pm
+++ lib/Actium/Cmd/Scratch.pm
@@ -2,28 +2,67 @@ package Actium::Cmd::Scratch 0.011;
 
 use Actium::Preamble;
 use Actium::O::2DArray;
+use Actium::O::Folder;
 
 # a place to test out small programs, in the Actium environment
 
 sub START {
 
-    my $new_p_records_r = [ [qw/a b c/], [qw/d e f/], ];
+    my $aoa = Actium::O::2DArray->new_from_file(
+        '/Users/apriven/Dropbox/flags_clusters.xlsx');
 
-    my $new_p_heads_r = [qw/H1 H2 H3/];
+    my %cluster_of;
+    my %stops_of;
 
-    my $aoa
-      = Actium::O::2DArray->new($new_p_records_r);
-      ;
-      
-    say u::dumpstr($aoa);
-    
-    my $tabbed
-      = Actium::O::2DArray->new($new_p_records_r)->tsv( @{$new_p_heads_r} )
-      ;
+    foreach \my @row(@$aoa) {
+        my $stopid = $row[0];
+        next if $stopid =~ /id/i;
+        my $cluster = $row[5];
 
-    say $tabbed;
+        push $stops_of{$cluster}->@*, $stopid;
 
-}
+    }
+
+    foreach my $cluster ( keys %stops_of ) {
+        my @stops = sort $stops_of{$cluster}->@*;
+        if ( scalar(@stops) <= 20 ) {
+            foreach my $stop (@stops) {
+                $cluster_of{$stop} = $cluster;
+            }
+        }
+        else {
+            my $subcluster = 0;
+            while (@stops) {
+                $subcluster++;
+                my @these20 = splice( @stops, 0, 20 );
+                foreach my $stop (@these20) {
+                    $cluster_of{$stop} = $cluster . $subcluster;
+                }
+            }
+
+        }
+
+    } ## tidy end: foreach my $cluster ( keys ...)
+
+    my $fname  = '/Users/apriven/Desktop/SaveAsEPS/PrintSet5';
+    my $folder = Actium::O::Folder->new($fname);
+
+    my @files = $folder->glob_files('*.eps');
+
+    my %new_of;
+
+    foreach my $file (@files) {
+        my $oname = u::filename($file);
+        $oname =~ m/(\d{5})(Fr|Bk)/;
+        my $stopid = $1;
+        my $side   = $2;
+        my $nname  = "$cluster_of{$stopid}-$stopid$side.eps";
+        say "$fname/$oname, $fname/$nname";
+        rename "$fname/$oname", "$fname/$nname";
+
+    }
+
+} ## tidy end: sub START
 
 1;
 
