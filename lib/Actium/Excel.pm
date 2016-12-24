a0b60c802a1d104cb735a774fa0c494f2b1195c6
Aaron Priven <aaron@priven.com>
2016-12-23 16:12:14 -0800

    ::Excel -- add new_workbook, actium_text_format
    Add new_workbook routine in Actium::Excel which makes it easier to
    use a preferred window size.
    
    Monkey patched new actium_text_format method to Excel::Writer::XLSX,
    making it slightly easier to add a simple text format.
    
    Renamed write_row_string to actium_write_row_string and write_col_string to
    actium_write_col_string which should eliminate possibility of conflicting
    names.

--- lib/Actium/Excel.pm
+++ lib/Actium/Excel.pm
@@ -5,34 +5,62 @@ use Actium::Preamble;
 use Excel::Writer::XLSX 0.95;               ## DEP ##
 use Excel::Writer::XLSX::Worksheet 0.95;    ## DEP ##
 
-# This exists purely as a way of monkeypatching Excel::Writer::XLSX.
-# It allows write_col_string and write_row_string to write
+use Sub::Exporter -setup => { exports => [qw(new_workbook)] };
+
+# This exists mostly as a way of monkeypatching Excel::Writer::XLSX.
+# It allows actium_write_col_string and actium_write_row_string to write
 # columns and rows specifically as strings.
 
 # I am not sure this is a particularly good way of going about this...
 
 const my $SHEET_CLASS => 'Excel::Writer::XLSX::Worksheet';
 
-#foreach my $method (qw/write_row_string write_col_string/) {
-#    die "$SHEET_CLASS Worksheet already has a method '$method'"
-#      if $SHEET_CLASS->can($method);
-#}
+const my $xlsx_window_height => 950;
+const my $xlsx_window_width  => 1200;
+
+sub new_workbook {
+
+    my $fh_or_fname;
+
+    my $workbook = Excel::Writer::XLSX->new($fh_or_fname);
+    if ( not defined $workbook ) {
+        if ( u::is_io_ref($fh_or_fname) ) {
+            croak "Couldn't create workbook";
+        }
+        else {
+            croak "Couldn't create workbook at $fh_or_fname";
+        }
+    }
+
+    $workbook->set_size( $xlsx_window_width, $xlsx_window_height );
+
+}
+
+###############################################################################
+### Monkey patching methods
+
+package Excel::Writer::XLSX;
+
+sub actium_text_format {
+    my $workbook = shift;
+    return $workbook->add_format( num_format => 0x31 );    # text only
+}
 
 package Excel::Writer::XLSX::Worksheet;
 
 ###############################################################################
 #
-# write_row_string($row, $col, $array_ref, $format)
+# actium_write_row_string($row, $col, $array_ref, $format)
 #
 # Write a row of strings starting from ($row, $col).
-# Call write_col_string() if any of
+# Call actium_write_col_string() if any of
 # the elements of the array ref are in turn array refs. This allows the writing
 # of 1D or 2D arrays of data in one go.
 #
 # Returns: the first encountered error value or zero for no errors
 #
 
-sub write_row_string {
+sub actium_write_row_string {
 
     my $self = shift;
 
@@ -43,7 +71,7 @@ sub write_row_string {
 
     # Catch non array refs passed by user.
     if ( ref $_[2] ne 'ARRAY' ) {
-        croak "Not an array ref in call to write_row_string()$!";
+        croak "Not an array ref in call to actium_write_row_string()$!";
     }
 
     my $row     = shift;
@@ -57,7 +85,7 @@ sub write_row_string {
 
         # Check for nested arrays
         if ( ref $token eq "ARRAY" ) {
-            $ret = $self->write_col_string( $row, $col, $token, @options );
+            $ret = $self->actium_write_col_string( $row, $col, $token, @options );
         }
         else {
             $token //= '';
@@ -70,21 +98,21 @@ sub write_row_string {
     }
 
     return $error;
-} ## tidy end: sub write_row_string
+} ## tidy end: sub actium_write_row_string
 
 ###############################################################################
 #
-# write_col_string ($row, $col, $array_ref, $format)
+# actium_write_col_string ($row, $col, $array_ref, $format)
 #
 # Write a column of strings starting from ($row, $col).
-# Call write_row_string() if any of
+# Call actium_write_row_string() if any of
 # the elements of the array ref are in turn array refs. This allows the writing
 # of 1D or 2D arrays of data in one go.
 #
 # Returns: the first encountered error value or zero for no errors
 #
 
-sub write_col_string {
+sub actium_write_col_string {
 
     my $self = shift;
 
@@ -95,7 +123,7 @@ sub write_col_string {
 
     # Catch non array refs passed by user.
     if ( ref $_[2] ne 'ARRAY' ) {
-        croak "Not an array ref in call to write_col_string()$!";
+        croak "Not an array ref in call to actium_write_col_string()$!";
     }
 
     my $row     = shift;
@@ -109,7 +137,7 @@ sub write_col_string {
 
         # Check for nested arrays
         if ( ref $token eq "ARRAY" ) {
-            $ret = $self->write_row_string( $row, $col, $token, @options );
+            $ret = $self->actium_write_row_string( $row, $col, $token, @options );
         }
         else {
             $token //= '';
