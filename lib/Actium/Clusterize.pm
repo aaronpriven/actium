bf6882915b71208642bf3ec27a485c665c4a6026
Aaron Priven <aaron@priven.com>
2017-03-15 17:19:19 -0700

    Added hash of leaves to unfold_clusters

--- lib/Actium/Clusterize.pm
+++ lib/Actium/Clusterize.pm
@@ -8,8 +8,8 @@ sub unfold_clusters {
 
     ( my $size, \my %count_of_leaf ) = _validate_unfold(@_);
 
-    \my ( @roots, %count_of_node, %children_of ) = _nodes( \%count_of_leaf );
-    # add %leaves_of
+    \my ( @roots, %count_of_node, %children_of, %leaves_of )
+      = _nodes( \%count_of_leaf );
 
     my @to_process = @roots;
     my @processed;
@@ -31,253 +31,250 @@ sub unfold_clusters {
             my $child = $children_of{$node}[$child_idx];
 
             if ( $count_of_node{$child} >= $size
-                and ( $count_of_node{$node} - $count_of_node{$child} ) >= $size )
+                and ( $count_of_node{$node} - $count_of_node{$child} )
+                >= $size )
             {
                 $count_of_node{$node} -= $count_of_node{$child};
                 push @processed, $child;
                 unshift @to_process, $node;
-                splice ($children_of{$node}->@*,$child_idx,1);
-                # also remove the leavf from leaves_of{node}
+                splice( $children_of{$node}->@*, $child_idx, 1 );
+                delete $leaves_of{$node}{$child};
             }
 
         }
 
     } ## tidy end: NODE: while (@to_process)
-    
+
     # now @processed is the list of clusters
     # need to return %leaves_of{@processed}
-    
-}
 
-    sub _validate_unfold {
-        my %params = validate(
-            @_,
-            {   size     => { default => 40 },
-                count_of => { type    => $PV_TYPE{HASHREF}, optional => 1 },
-                clusters => { type    => $PV_TYPE{ARRAYREF}, optional => 1 },
-            }
-        );
+} ## tidy end: sub unfold_clusters
 
-        my %count_of;
-
-        if ( exists $params{clusters} ) {
-            croak "Cannot specify both count_of and clusters"
-              if exists $params{count_of};
-            $count_of{$_}++ foreach ( $params{clusters}->@* );
+sub _validate_unfold {
+    my %params = validate(
+        @_,
+        {   size     => { default => 40 },
+            count_of => { type    => $PV_TYPE{HASHREF}, optional => 1 },
+            clusters => { type    => $PV_TYPE{ARRAYREF}, optional => 1 },
         }
-        elsif ( exists $params{count_of} ) {
-            \%count_of = $params{count_of};
+    );
+
+    my %count_of;
+
+    if ( exists $params{clusters} ) {
+        croak "Cannot specify both count_of and clusters"
+          if exists $params{count_of};
+        $count_of{$_}++ foreach ( $params{clusters}->@* );
+    }
+    elsif ( exists $params{count_of} ) {
+        \%count_of = $params{count_of};
+    }
+    else {
+        croak "Must specify either count_of or clusters";
+    }
+
+    return $params{size}, \%count_of;
+} ## tidy end: sub _validate_unfold
+
+sub _nodes {
+
+    \my %count_of_leaf = shift;
+
+    my $length = u::max( map {length} keys %count_of_leaf );
+
+    my ( %count_of_node, %leaves_of, %is_a_root, %children_of );
+
+    foreach my $leaf ( keys %count_of_leaf ) {
+        my @chars = split( //, $leaf );
+        if ( @chars < $length ) {
+            push @chars, " " x ( $length - @chars );
         }
-        else {
-            croak "Must specify either count_of or clusters";
-        }
+        
+        my $leafcount = $count_of_leaf{$leaf};
 
-        return $params{size}, \%count_of;
-    } ## tidy end: sub _validate_unfold
+        while ( @chars > 1 ) {
 
-    sub _nodes {
+            my $node = join( $EMPTY, @chars );
+            $count_of_node{$node} += $leafcount;
+            $leaves_of{$node}{$leaf} = 1;
 
-        \my %count_of_leaf = shift;
+            pop @chars;
 
-        my $length = u::max( map {length} keys %count_of_leaf );
+            my $parent = join( $EMPTY, @chars );
+            push $children_of{$parent}->@*, $node;
 
-        my ( %count_of_node, %is_a_root, %children_of );
-
-        foreach my $leaf ( keys %count_of_leaf ) {
-            my @chars = split( //, $leaf );
-            if ( @chars < $length ) {
-                push @chars, " " x ( $length - @chars );
-            }
-
-            my $leafcount = $count_of_leaf{$leaf};
-
-            while ( @chars > 1 ) {
-
-                my $node = join( $EMPTY, @chars );
-                $count_of_node{$node} += $leafcount;
-
-                pop @chars;
-
-                my $parent = join( $EMPTY, @chars );
-                push $children_of{$parent}->@*, $node;
-
-            }
-
-            $is_a_root{ $chars[0] } = 1;
-            $count_of_node{ $chars[0] } += $leafcount;
-
-        } ## tidy end: foreach my $leaf ( keys %count_of_leaf)
-
-        return [ keys %is_a_root ], \%count_of_node, \%children_of;
-
-    } ## tidy end: sub _nodes
-
-    const my $CLUSTER_MINIMUM => 10;
-
-    # all clusters must be the same length
-    # and all characters must be digits except the first one
-
-    sub fold_clusters {
-
-        # accepts a hashref of cluster counts, an arrayref of clusters,
-        # or a list of clusters
-
-        my %count_of;
-        my $ref = shift;
-
-        if ( u::is_hashref($ref) ) {
-            \%count_of = $ref;
-        }
-        elsif ( u::is_arrayref($ref) ) {
-            $count_of{$_}++ foreach $ref->@*;
-        }
-        elsif ( u::is_ref($ref) ) {
-            croak "Invalid "
-              . u::reftype($ref)
-              . " reference passed to clusterize";
-        }
-        else {
-            $count_of{$_}++ foreach ( $ref, @_ );
         }
 
-        my %leaves_of = map { $_, [$_] } keys %count_of;
-        # this represents the leaves that have been folded into this cluster.
+        $is_a_root{ $chars[0] } = 1;
+        $count_of_node{ $chars[0] } += $leafcount;
 
-        my %folded_of = map { $_, $_ } keys %count_of;
-        # this represents the ancestor that this cluster has been folded into.
+    } ## tidy end: foreach my $leaf ( keys %count_of_leaf)
 
-        # so, for example,
+    return [ keys %is_a_root ], \%count_of_node, \%children_of, \%leaves_of;
 
-        # $leaves_of{15} = [ 151, 152, 154 ]
-        #    and
-        # $folded_of{$151} = 15
-        # $folded_of{$152} = 15
-        # $folded_of{$153} = 15
+} ## tidy end: sub _nodes
 
-        # Before folding, all clusters point to themselves
+const my $CLUSTER_MINIMUM => 10;
 
-        my $fold_cr = sub {
-            my $child  = shift;
-            my $parent = shift;
+# all clusters must be the same length
+# and all characters must be digits except the first one
 
-            if ( not defined $parent ) {
-                my @digits = split( //, $child );
-                my $lastdigit = pop @digits;
-                $parent = join( $EMPTY, @digits );
-            }
+sub fold_clusters {
 
-            $count_of{$parent} += $count_of{$child};
-            delete $count_of{$child};
+    # accepts a hashref of cluster counts, an arrayref of clusters,
+    # or a list of clusters
 
-            my @leaves = $leaves_of{$child}->@*;
-            push $leaves_of{$parent}->@*, @leaves;
-            delete $leaves_of{$child};
+    my %count_of;
+    my $ref = shift;
 
-            foreach my $leaf (@leaves) {
-                $folded_of{$leaf} = $parent;
-            }
+    if ( u::is_hashref($ref) ) {
+        \%count_of = $ref;
+    }
+    elsif ( u::is_arrayref($ref) ) {
+        $count_of{$_}++ foreach $ref->@*;
+    }
+    elsif ( u::is_ref($ref) ) {
+        croak "Invalid " . u::reftype($ref) . " reference passed to clusterize";
+    }
+    else {
+        $count_of{$_}++ foreach ( $ref, @_ );
+    }
 
-        };
+    my %leaves_of = map { $_, [$_] } keys %count_of;
+    # this represents the leaves that have been folded into this cluster.
 
-        my $folded;
-        do {
-            $folded = 0;
-            # first, move anything less than ten down a level,
-            # unless it's already at the top level.
-            foreach my $leaf ( sort keys %count_of ) {
+    my %folded_of = map { $_, $_ } keys %count_of;
+    # this represents the ancestor that this cluster has been folded into.
 
-                next
-                  if length($leaf) == 1
-                  or $count_of{$leaf} >= $CLUSTER_MINIMUM;
+    # so, for example,
 
-                # so move this one down
-                $fold_cr->($leaf);
-                $folded = 1;
+    # $leaves_of{15} = [ 151, 152, 154 ]
+    #    and
+    # $folded_of{$151} = 15
+    # $folded_of{$152} = 15
+    # $folded_of{$153} = 15
 
-            }
+    # Before folding, all clusters point to themselves
 
-            # Then, if a cluster still has less than ten,
-            # yank down the cluster whose average descendant value
-            # is closest to that of its own descendants
+    my $fold_cr = sub {
+        my $child  = shift;
+        my $parent = shift;
 
-            my @clusters = sort keys %count_of;
-            foreach my $cluster (@clusters) {
+        if ( not defined $parent ) {
+            my @digits = split( //, $child );
+            my $lastdigit = pop @digits;
+            $parent = join( $EMPTY, @digits );
+        }
 
-                next unless defined $count_of{$cluster};   # already been yanked
-                next if $count_of{$cluster} >= $CLUSTER_MINIMUM;
+        $count_of{$parent} += $count_of{$child};
+        delete $count_of{$child};
 
-                # find non-folded children
-                my @children = grep /^${cluster}[0-9]+/, @clusters;
-                next unless @children;
+        my @leaves = $leaves_of{$child}->@*;
+        push $leaves_of{$parent}->@*, @leaves;
+        delete $leaves_of{$child};
 
-                my $cluster_average
-                  = u::mean( map { substr( $_, 1 ) } $leaves_of{$cluster}->@* );
+        foreach my $leaf (@leaves) {
+            $folded_of{$leaf} = $parent;
+        }
 
-                my %closeness;
+    };
 
-                foreach my $child (@children) {
-                    my $leaf_average
-                      = u::mean( map { substr( $_, 1 ) }
-                          $leaves_of{$child}->@* );
-                    $closeness{$child}
-                      = abs( $leaf_average - $cluster_average );
-                }
+    my $folded;
+    do {
+        $folded = 0;
+        # first, move anything less than ten down a level,
+        # unless it's already at the top level.
+        foreach my $leaf ( sort keys %count_of ) {
 
-                @children
-                  = sort { $closeness{$a} <=> $closeness{$b} } @children;
-
-                foreach my $child (@children) {
-                    $fold_cr->( $child, $cluster );
-                    last if $count_of{$cluster} >= $CLUSTER_MINIMUM;
-                }
-
-            } ## tidy end: foreach my $cluster (@clusters)
-        } while $folded;
-
-        # now go back and de-generalize where possible
-
-        foreach my $cluster ( keys %count_of ) {
             next
-              if defined $folded_of{$cluster}
-              and $folded_of{$cluster} == $cluster;
+              if length($leaf) == 1
+              or $count_of{$leaf} >= $CLUSTER_MINIMUM;
 
-            my @leaves = $leaves_of{$cluster}->@*;
-
-            if ( @leaves == 1 ) {
-                $folded_of{ $leaves[0] } = $leaves[0];
-                delete $folded_of{$cluster};
-                next;
-            }
-
-            my @parents = @leaves;
-
-            while ( length( $parents[0] ) > 1 ) {
-                chop @parents;
-
-                if ( ( scalar( u::uniq @parents ) ) == 1 ) {
-                    $folded_of{$_} = $parents[0] foreach @leaves;
-                    last;
-                }
-            }
-
-        } ## tidy end: foreach my $cluster ( keys ...)
-
-        # put the x's on the end
-
-        foreach my $leaf ( keys %folded_of ) {
-            my $folded       = $folded_of{$leaf};
-            my $leaflength   = length($leaf);
-            my $foldedlength = length($folded);
-            if ( $leaflength > $foldedlength ) {
-                my $folded
-                  = $folded . ( 'x' x ( $leaflength - $foldedlength ) );
-                $folded_of{$leaf} = $folded;
-            }
+            # so move this one down
+            $fold_cr->($leaf);
+            $folded = 1;
 
         }
 
-        return %folded_of;
+        # Then, if a cluster still has less than ten,
+        # yank down the cluster whose average descendant value
+        # is closest to that of its own descendants
 
-    } ## tidy end: sub fold_clusters
+        my @clusters = sort keys %count_of;
+        foreach my $cluster (@clusters) {
 
-    1;
+            next unless defined $count_of{$cluster};    # already been yanked
+            next if $count_of{$cluster} >= $CLUSTER_MINIMUM;
+
+            # find non-folded children
+            my @children = grep /^${cluster}[0-9]+/, @clusters;
+            next unless @children;
+
+            my $cluster_average
+              = u::mean( map { substr( $_, 1 ) } $leaves_of{$cluster}->@* );
+
+            my %closeness;
+
+            foreach my $child (@children) {
+                my $leaf_average
+                  = u::mean( map { substr( $_, 1 ) } $leaves_of{$child}->@* );
+                $closeness{$child} = abs( $leaf_average - $cluster_average );
+            }
+
+            @children
+              = sort { $closeness{$a} <=> $closeness{$b} } @children;
+
+            foreach my $child (@children) {
+                $fold_cr->( $child, $cluster );
+                last if $count_of{$cluster} >= $CLUSTER_MINIMUM;
+            }
+
+        } ## tidy end: foreach my $cluster (@clusters)
+    } while $folded;
+
+    # now go back and de-generalize where possible
+
+    foreach my $cluster ( keys %count_of ) {
+        next
+          if defined $folded_of{$cluster}
+          and $folded_of{$cluster} == $cluster;
+
+        my @leaves = $leaves_of{$cluster}->@*;
+
+        if ( @leaves == 1 ) {
+            $folded_of{ $leaves[0] } = $leaves[0];
+            delete $folded_of{$cluster};
+            next;
+        }
+
+        my @parents = @leaves;
+
+        while ( length( $parents[0] ) > 1 ) {
+            chop @parents;
+
+            if ( ( scalar( u::uniq @parents ) ) == 1 ) {
+                $folded_of{$_} = $parents[0] foreach @leaves;
+                last;
+            }
+        }
+
+    } ## tidy end: foreach my $cluster ( keys ...)
+
+    # put the x's on the end
+
+    foreach my $leaf ( keys %folded_of ) {
+        my $folded       = $folded_of{$leaf};
+        my $leaflength   = length($leaf);
+        my $foldedlength = length($folded);
+        if ( $leaflength > $foldedlength ) {
+            my $folded = $folded . ( 'x' x ( $leaflength - $foldedlength ) );
+            $folded_of{$leaf} = $folded;
+        }
+
+    }
+
+    return %folded_of;
+
+} ## tidy end: sub fold_clusters
+
+1;
