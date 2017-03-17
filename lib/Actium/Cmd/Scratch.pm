13077fc942e48be63ec154a403678438435a8899
Aaron Priven <aaron@priven.com>
2017-03-16 17:44:31 -0700

    Clusterize: bugs, documentation
    Added partitions 4+ zones when it's bigger than the size.
    
    Fixed the bug in Actium::Clusterize that meant it didn't work for levels deeper
    than three. Now it should work with any level, and allow variable length
    roots, including 0.
    
    Wrote Actium::Clusterize pod.
    
    Eliminated old Scratch material.

--- lib/Actium/Cmd/Scratch.pm
+++ lib/Actium/Cmd/Scratch.pm
@@ -2,7 +2,7 @@ package Actium::Cmd::Scratch 0.012;
 
 use Actium::Preamble;
 
-use Actium::Clusterize('unfold_clusters');
+use Actium::Clusterize('clusterize');
 
 sub START {
 
@@ -26,93 +26,20 @@ sub START {
         502 => 4,
         510 => 73,
         607 => 8,
+        701 => 21,
+        702 => 21,
+        703 => 21,
+        704 => 41,
+        705 => 21,
     );
-    
-    \my %unfolded_of = unfold_clusters( count_of => \%count_of);
-    
+
+    \my %unfolded_of
+      = clusterize( count_of => \%count_of, root_digits => 1 );
+
     print STDERR "%unfolded_of = ";
-    
+
     use DDP;
     p %unfolded_of;
-
-}
-
-1;
-
-__END__
-
-
-use Actium::O::2DArray;
-use Actium::O::Folder;
-
-# a place to test out small programs, in the Actium environment
-
-sub START {
-
-    my $aoa = Actium::O::2DArray->new_from_file(
-        '/Users/apriven/Dropbox/flags_clusters.xlsx');
-
-    my %cluster_of;
-    my %stops_of;
-
-    foreach \my @row(@$aoa) {
-        my $stopid = $row[0];
-        next if $stopid =~ /id/i;
-        my $cluster = $row[5];
-
-        push $stops_of{$cluster}->@*, $stopid;
-
-    }
-
-    my %is_subcluster;
-
-    foreach my $cluster ( keys %stops_of ) {
-        my @stops = sort $stops_of{$cluster}->@*;
-        if ( scalar(@stops) <= 20 ) {
-            foreach my $stop (@stops) {
-                $cluster_of{$stop}       = $cluster;
-                $is_subcluster{$cluster} = 1;
-            }
-        }
-        else {
-            my $subcluster = 0;
-            while (@stops) {
-                $subcluster++;
-                my @these20 = splice( @stops, 0, 20 );
-                foreach my $stop (@these20) {
-                    my $finalcluster = $cluster . $subcluster;
-                    $cluster_of{$stop}            = $finalcluster;
-                    $is_subcluster{$finalcluster} = 1;
-                }
-            }
-
-        }
-
-    } ## tidy end: foreach my $cluster ( keys ...)
-
-    my $fname  = '/Users/apriven/Desktop/SaveAsEPS/PrintSet7';
-    my $folder = Actium::O::Folder->new($fname);
-
-    my @files = $folder->glob_files('*.eps');
-
-    my %new_of;
-
-    foreach my $cluster ( keys %is_subcluster ) {
-        my $dir = "$fname/$cluster";
-        mkdir "$fname/$cluster" unless -d $dir;
-    }
-
-    foreach my $file (@files) {
-        my $oname = u::filename($file);
-        $oname =~ m/(\d{5})(Fr|Bk)/;
-        my $stopid  = $1;
-        my $side    = $2;
-        my $cluster = $cluster_of{$stopid};
-        my $nname   = "$cluster/$cluster-$stopid$side.eps";
-        say "$fname/$oname, $fname/$nname";
-        rename "$fname/$oname", "$fname/$nname";
-
-    }
 
 } ## tidy end: sub START
 
