6a07b8fccdda579b75fdc5b5cc904a99f6fd3dfe
Aaron Priven <aaron@priven.com>
2017-04-21 14:49:48 -0700

    Add POD or copyright templates to all files

--- lib/Actium/O/Sked/Timetable/IDPageFrameSets.pm
+++ lib/Actium/O/Sked/Timetable/IDPageFrameSets.pm
@@ -440,43 +440,76 @@ __PACKAGE__->meta->make_immutable;
 
 __END__
 
+=encoding utf8
 
-my ( @heights, @compressed_heights );
-my $widest            = 0;
-my $widest_compressed = 0;
+=head1 NAME
 
-my %widest_frameset_of;
-my %widest_compressed_frameset_of;
+<name> - <brief description>
 
-for my $frameset_r (@page_framesets) {
+=head1 VERSION
 
-    my $height = $frameset_r->{height};
-    my $width  = $frameset_r->{width};
+This documentation refers to version 0.003
 
-    if ( not exists $widest_frameset_of{$height}
-        or $width > $widest_frameset_of{$height}{width} )
-    {
-        $widest_frameset_of{$height} = $frameset_r;
-    }
+=head1 SYNOPSIS
 
-    $widest = max( $widest, $frameset_r->{width} );
+ use <name>;
+ # do something with <name>
+   
+=head1 DESCRIPTION
 
-}
+A full description of the module and its features.
 
-for my $frameset_r (@compressed_framesets) {
+=head1 SUBROUTINES or METHODS (pick one)
 
-    my $height = $frameset_r->{height};
-    my $width  = $frameset_r->{width};
+=over
 
-    if ( not exists $widest_compressed_frameset_of{$height}
-        or $width > $widest_compressed_frameset_of{$height}{width} )
-    {
-        $widest_compressed_frameset_of{$height} = $frameset_r;
-    }
+=item B<subroutine()>
 
-    $widest_compressed = max( $widest, $frameset_r->{width} );
+Description of subroutine.
 
-}
+=back
 
-@heights            = keys %widest_frameset_of;
-@compressed_heights = keys %widest_compressed_frameset_of;
+=head1 DIAGNOSTICS
+
+A list of every error and warning message that the application can
+generate (even the ones that will "never happen"), with a full
+explanation of each problem, one or more likely causes, and any
+suggested remedies. If the application generates exit status codes,
+then list the exit status associated with each error.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+A full explanation of any configuration system(s) used by the
+application, including the names and locations of any configuration
+files, and the meaning of any environment variables or properties
+that can be se. These descriptions must also include details of any
+configuration language used.
+
+=head1 DEPENDENCIES
+
+List its dependencies.
+
+=head1 AUTHOR
+
+Aaron Priven <apriven@actransit.org>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2017
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of either:
+
+=over 4
+
+=item * the GNU General Public License as published by the Free
+Software Foundation; either version 1, or (at your option) any
+later version, or
+
+=item * the Artistic License version 2.0.
+
+=back
+
+This program is distributed in the hope that it will be useful, but WITHOUT 
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+FITNESS FOR A PARTICULAR PURPOSE.
