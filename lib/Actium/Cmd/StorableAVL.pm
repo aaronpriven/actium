d81a37b0dcfe20d056621b78d8799e49a90ac305
Aaron Priven <aaron@priven.com>
2017-04-27 16:19:02 -0700

    Fix POD errors in StorableAVL and Util

--- lib/Actium/Cmd/StorableAVL.pm
+++ lib/Actium/Cmd/StorableAVL.pm
@@ -394,6 +394,8 @@ This program is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
 FITNESS FOR A PARTICULAR PURPOSE.
 
+=cut
+
 1;
 
 __DATA__
