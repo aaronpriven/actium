ba37ac6ac40a34d9b98688bb2f677e954fdeb80c
Aaron Priven <aaron@priven.com>
2017-05-24 14:05:38 -0700

    Replace Actium::Preamble with Actium.pm
    Also tidied

--- lib/Actium/O/Cmd/Option.pm
+++ lib/Actium/O/Cmd/Option.pm
@@ -1,6 +1,6 @@
 package Actium::O::Cmd::Option 0.011;
 
-use Actium::Moose;
+use Actium ('class_nomod');
 
 # it will look for, in this order:
 
@@ -29,10 +29,10 @@ around BUILDARGS => sub {
             description     => 1,
             display_default => { type => $PV_TYPE{BOOLEAN}, default => 0 },
 
-            order => { type => $PV_TYPE{SCALAR} },
-            spec       => 1,
-            callback   => 0,
-            prompt     => 0,
+            order    => { type => $PV_TYPE{SCALAR} },
+            spec     => 1,
+            callback => 0,
+            prompt   => 0,
             no_command => { type => $PV_TYPE{BOOLEAN}, default => 0 },
             prompthide => { type => $PV_TYPE{BOOLEAN}, default => 0 },
         }
@@ -206,8 +206,8 @@ then list the exit status associated with each error.
 
 A full explanation of any configuration system(s) used by the
 application, including the names and locations of any configuration
-files, and the meaning of any environment variables or properties
-that can be se. These descriptions must also include details of any
+files, and the meaning of any environment variables or properties that
+can be se. These descriptions must also include details of any
 configuration language used.
 
 =head1 DEPENDENCIES
@@ -222,8 +222,8 @@ Aaron Priven <apriven@actransit.org>
 
 Copyright 2017
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of either:
+This program is free software; you can redistribute it and/or modify it
+under the terms of either:
 
 =over 4
 
@@ -235,6 +235,7 @@ later version, or
 
 =back
 
-This program is distributed in the hope that it will be useful, but WITHOUT 
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-FITNESS FOR A PARTICULAR PURPOSE.
+This program is distributed in the hope that it will be useful, but
+WITHOUT  ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE.
+
