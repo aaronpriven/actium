be543b67af619fad8cd9d8b6b3acbae1c11ed0d4
Aaron Priven <aaron@priven.com>
2017-07-05 15:56:53 -0700

    Use folder() from Actium::O::Folders::Signup
    Replace direct call to Storable with MooseX::Storage
    
    Add name and signup attributes
    
    Complete finalize_skeds

--- lib/Actium/O/Sked/Collection.pm
+++ lib/Actium/O/Sked/Collection.pm
@@ -7,17 +7,15 @@ use Actium::Sorting::Skeds ('skedsort');
 
 use Actium::Excel;
 
-around BUILDARGS ($orig, $class: slurpy @ ) {
+use MooseX::Storage;
 
-    # if they're all objects, treat them as though they were skeds
-    if ( u::all { u::is_blessed_ref($_) } @_ ) {
-        return $class->$orig( skeds => \@_ );
-    }
+with Storage(
+    traits   => ['OnlyWhenBuilt'],
+    'format' => 'Storable',
+    io       => 'File',
+);
 
-    # otherwise, normal
-    return $class->$orig(@_);
-
-}
+const my $PHYLUM => 's';
 
 has skeds_r => (
     is       => 'ro',
@@ -29,10 +27,23 @@ has skeds_r => (
     handles  => { skeds => 'elements' },
 );
 
+has name => (
+    is       => 'rwp',
+    isa      => 'Str',
+    required => 1,
+    traits   => ['DoNotSerialize'],
+);
+
+has signup => (
+    is       => 'rwp',
+    isa      => 'Actium::O::Folders::Signup',
+    required => 1,
+    traits   => ['DoNotSerialize']
+);
+
 sub BUILD {
     my $self  = shift;
     my @skeds = skedsort( $self->skeds );
-
     return $self->_set_skeds_r( \@skeds );
 }
 
@@ -45,7 +56,8 @@ has '_sked_obj_by_id_r' => (
     handles => {
         _set_sked_obj => 'set',
         sked_obj      => 'get',
-        #ids => 'keys',
+        _sked_ids     => 'keys',
+        _has_sked_id  => 'exists',
     },
 );
 
@@ -112,15 +124,11 @@ sub _build_sked_ids_of_lg {
     my @skeds = $self->skeds;
     my %sked_ids_of_lg;
     foreach my $sked (@skeds) {
-
         my $id        = $sked->id;
         my $linegroup = $sked->linegroup;
         push $sked_ids_of_lg{$linegroup}->@*, $id;
-
     }
-
     return \%sked_ids_of_lg;
-
 }
 
 sub sked_ids_of_lg {
@@ -137,35 +145,31 @@ sub skeds_of_lg {
     return @skeds;
 }
 
-#sub add_sked {
-#    my $self = shift;
-#    my @objs = shift;
-#    $self->_set_sked_obj( map { $_->id, $_ } @objs );
-#}
-
 ##################
 ##### INPUT ######
 ##################
 
-sub load_raw_storable {
-    my $class           = shift;
-    my $storable_folder = shift;
-    return $storable_folder->retrieve('rawskeds.storable');
+method load_storable ($class: :$signup, :$collection) {
+
+    my $folder
+      = $signup->folder( phylum => $PHYLUM, collection => $collection );
+
+    my $self = $class->load( $folder->make_filespec('skeds_storable') );
+    $self->_set_signup($signup);
+    $self->_set_name($collection);
+    return $self;
+
 }
 
-sub load_storable {
-    my $class           = shift;
-    my $storable_folder = shift;
-    return $storable_folder->retrieve('skeds.storable');
-}
+method load_xlsx ( $class: :$signup, :$collection ) {
 
-method load_xlsx (
-        $class:
-        Actium::O::Folder :$xlsx_folder ,
-        Actium::O::Files::ActiumDB :$actiumdb ,
-    ) {
+    my $folder = $signup->folder(
+        phylum     => $PHYLUM,
+        collection => $collection,
+        format     => 'skeds'
+    );
 
-    my @xlsx_files = $xlsx_folder->glob_plain_files('*.xlsx');
+    my @xlsx_files = $folder->glob_plain_files('*.xlsx');
 
     my @skeds;
     foreach my $file (@xlsx_files) {
@@ -173,25 +177,50 @@ method load_xlsx (
         push @skeds, $sked;
     }
 
-    return $class->new(@skeds);
+    return $class->new(
+        skeds  => \@skeds,
+        name   => $collection,
+        signup => $signup
+    );
 
-}
+} ## tidy end: method load_xlsx
 
-method cook_skeds (
-    $class:
-    Actium::O::Folder :$signup ,
-    Actium::O::Files::ActiumDB :$actiumdb ,
-    ) {
+#######################
+##### TRANSFORMATION
+#######################
 
-    my $main_collection
-      = $class->load_raw_storable( $signup->skeds_base_folder );
+method finalize_skeds ( $class: :$signup ) {
+
+    my $received_collection
+      = $class->load_storable( signup => $signup, collection => 'received' );
 
     my $exception_collection
-      = $class->load_xlsx( $signup->exceptions_skeds_folder );
+      = $class->load_xlsx( signup => $signup, collection => 'exceptions' );
 
-    # TODO - go through exceptions and replace sked in main collection
+    my @finalized_skeds;
 
-}
+    my @ids
+      = uniq( $received_collection->_sked_ids,
+        $exception_collection->_sked_ids );
+
+    for my $id (@ids) {
+        if ( $exception_collection->_has_sked_id($id) ) {
+            push @finalized_skeds, $exception_collection->sked_obj($id);
+        }
+        else {
+            push @finalized_skeds, $received_collection->sked_obj($id);
+        }
+    }
+
+    my $finalized_collection = $class->new(
+        skeds  => \@finalized_skeds,
+        name   => 'final',
+        signup => $signup,
+    );
+
+    $finalized_collection->output_skeds_all;
+
+} ## tidy end: method finalize_skeds
 
 ###################
 ##### OUTPUT ######
@@ -229,11 +258,26 @@ sub write_tabxchange {
     $destination_code->store;
 } ## tidy end: sub write_tabxchange
 
-method output_skeds_dump ( :$dump_folder ) {
+method folder ($the_format) {
+    # calling it $format yields syntax formatting errors
+    # This can only be used for output folders since it depends on
+    # object attributes
+    my $signup     = $self->signup;
+    my $collection = $self->name;
+    return $signup->folder(
+        phylum     => $PHYLUM,
+        collection => $collection,
+        format     => $the_format,
+    );
+}
+
+method output_skeds_dump ( :$signup! ) {
 
     my $skeds_r = $self->skeds_r;
 
-    $dump_folder->write_files_with_method(
+    my $folder = $self->folder('dumped');
+
+    $folder->write_files_with_method(
         OBJECTS   => $skeds_r,
         METHOD    => 'dump',
         EXTENSION => 'dump',
@@ -241,42 +285,50 @@ method output_skeds_dump ( :$dump_folder ) {
 
 }
 
-method output_skeds_all ( :$signup! , :$subfolder_name = 's') {
+method output_skeds_all {
 
     my $skeds_r = $self->skeds_r;
+    my $signup  = $self->signup;
 
-    my $skeds_folder = $signup->subfolder($subfolder_name);
+    $self->output_skeds_storable;
 
-    $skeds_folder->store( $self, 'skeds.storable' );
-    my $xlsxfolder = $skeds_folder->subfolder('xlsx_s');
-    $xlsxfolder->write_files_with_method(
+    my $skeds_folder = $self->folder('skeds');
+
+    $skeds_folder->write_files_with_method(
         OBJECTS   => $skeds_r,
         METHOD    => 'xlsx',
         EXTENSION => 'xlsx',
     );
 
-    $self->output_skeds_xlsx( skeds_folder => $skeds_folder );
+    $self->output_skeds_place;
 
-    $self->output_skeds_dump( $skeds_folder->subfolder('dump') );
+    $self->output_skeds_dump;
 
-    my $spacedfolder = $skeds_folder->subfolder('spaced');
+    my $spacedfolder = $self->folder('spaced');
     $spacedfolder->write_files_with_method(
         OBJECTS   => $skeds_r,
         METHOD    => 'spaced',
         EXTENSION => 'txt',
     );
 
-    Actium::O::Sked->write_prehistorics( $skeds_r,
-        $skeds_folder->subfolder('prehistoric') );
+    my $prehistoricfolder = $self->folder('prehistoric');
+
+    Actium::O::Sked->write_prehistorics( $skeds_r, $prehistoricfolder );
 
 } ## tidy end: method output_skeds_all
 
-method output_skeds_xlsx (:$skeds_folder!) {
+method output_skeds_storable (:$signup!) {
+    my $filespec = $self->folder->make_filespec('skeds.storable');
+    $self->store($filespec);
+}
+
+method output_skeds_place {
 
     my $cry = cry('Writing place xlsx schedules');
 
     #my $stop_xlsx_folder  = $skeds_folder->subfolder('xlsx_s');
-    my $place_xlsx_folder = $skeds_folder->subfolder('xlsx_p');
+    #my $place_xlsx_folder = $skeds_folder->subfolder('xlsx_p');
+    my $place_xlsx_folder = $self->folder('place');
 
     my @linegroups = u::sortbyline $self->linegroups;
 
@@ -312,7 +364,7 @@ method output_skeds_xlsx (:$skeds_folder!) {
 
     return;
 
-} ## tidy end: method output_skeds_xlsx
+} ## tidy end: method output_skeds_place
 
 u::immut;
 
