097f7ff7812f95ac7c1f3340e909de7f38acd093
Aaron Priven <aaron@priven.com>
2017-07-10 23:32:27 -0700

    Rename ::Xhea to ::Import::Xhea

--- lib/Actium/Cmd/Xhea2Skeds.pm
+++ lib/Actium/Cmd/Xhea2Skeds.pm
@@ -3,7 +3,7 @@ package Actium::Cmd::Xhea2Skeds 0.012;
 # Takes tab files that are result of XheaImport, and makes schedules
 
 use Actium;
-use Actium::Xhea::ToSkeds;
+use Actium::Import::Xhea::ToSkeds;
 
 sub OPTIONS {
     return ( 'signup', 'actiumdb' );
@@ -15,7 +15,7 @@ sub START {
     my $signup   = $env->signup;
     my $actiumdb = $env->actiumdb;
 
-    Actium::Xhea::ToSkeds::xheatab2skeds(
+    Actium::Import::Xhea::ToSkeds::xheatab2skeds(
         actiumdb => $actiumdb,
         signup   => $signup,
     );
