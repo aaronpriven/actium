889eeb5853071e17d0217eb485516682ade4c55c
Aaron Priven <aaron@priven.com>
2017-07-18 23:26:32 -0700

    Use O::Time not Time in Actium::O::Sked::Timetable

--- lib/Actium/O/Sked/Timetable.pm
+++ lib/Actium/O/Sked/Timetable.pm
@@ -5,16 +5,12 @@ package Actium::O::Sked::Timetable 0.012;
 
 use Actium ('class');
 
-use Actium::Time;
+use Actium::O::Time;
 use Actium::Text::InDesignTags;
 
 const my $idt => 'Actium::Text::InDesignTags';
 
 use HTML::Entities;    ### DEP ###
-
-my $timesub = Actium::Time::timestr_sub();
-
-# Uses default values. Someday it would be nice to make that configurable
 
 has sked_obj => (
     isa      => 'Actium::O::Sked',
@@ -241,7 +237,7 @@ sub new_from_sked {
         }
 
         foreach my $timenum ( $trip->placetimes ) {
-            push @row, $timesub->($timenum);
+            push @row, Actium::O::Time->from_num($timenum)->ap;
         }
 
         push @body_rows, \@row;
