8418c806b921db8c51d41a09bfba147cabe299e4
Aaron Priven <aaron@priven.com>
2018-01-16 13:21:36 -0800

    Add copyright notices to test files

--- t/lib/Actium/Mock/Class.pm
+++ t/lib/Actium/Mock/Class.pm
@@ -30,3 +30,28 @@ package Actium::Mock::Class::WithLayers {
     sub meth_layers {':encoding(iso-8859-15)'}
 }
 
+1;
+
+__END__
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2018
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either:
+
+=over 4
+
+=item * the GNU General Public License as published by the Free
+Software Foundation; either version 1, or (at your option) any
+later version, or
+
+=item * the Artistic License version 2.0.
+
+=back
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT  ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE.
+
