b0086a68c9bfdaca4001909b9dc4ed8b51de4c5b
Aaron Priven <aaron@priven.com>
2018-01-31 17:27:35 -0800

    Added some documentation for act-pvt.pl

--- bin/act-pvt.pl
+++ bin/act-pvt.pl
@@ -34,68 +34,47 @@ Actium::Env::CLI::->new(
 
 __END__
 
-
-
 =encoding utf8
 
 =head1 NAME
 
-<name> - <brief description>
+act-pvt.pl - Private (or at least, not particularly public) commands in
+the Actium system
 
 =head1 VERSION
 
-This documentation refers to <name> version 0.003
+This documentation refers to <name> version 0.015
 
 =head1 USAGE
 
- # brief working invocation example(s) using the most comman usage(s)
-
-=head1 REQUIRED ARGUMENTS
-
-A list of every argument that must appear on the command line when the
-application is invoked, explaining what each one does, any restrictions
-on where each one may appear (i.e., flags that must appear before or
-after filenames), and how the various arguments and options may
-interact (e.g., mutual exclusions, required combinations, etc.)
-
-If all of the application's arguments are optional, this section may be
-omitted entirely.
-
-=over
-
-=item B<argument()>
-
-Description of argument.
-
-=back
-
-=head1 OPTIONS
-
-A complete list of every available option with which the application
-can be invoked, explaining wha each does and listing any restrictions
-or interactions.
-
-If the application has no options, this section may be omitted.
+ act-pvt.pl subcommand ...
 
 =head1 DESCRIPTION
 
-A full description of the program and its features.
+The act-pvt.pl command gives command-line access to various subcommands
+that are not considered public enough to make available to all Actium
+users. Generally they are commands used for development or informal
+testing.
 
-=head1 DIAGNOSTICS
+=head1 AVAILABLE SUBCOMMANDS
 
-A list of every error and warning message that the application can
-generate (even the ones that will "never happen"), with a full
-explanation of each problem, one or more likely causes, and any
-suggested remedies. If the application generates exit status codes,
-then list the exit status associated with each error.
+See the documentation in each subcommand's module file for more
+information.
 
-=head1 CONFIGURATION AND ENVIRONMENT
+=head2 help
 
-A full explanation of any configuration system(s) used by the
-application, including the names and locations of any configuration
-files, and the meaning of any environment variables or properties that
-can be se. These descriptions must also include details of any
-configuration language used.
+This provides a list of subcommands available using act-pvt.pl.
+
+=head2 time
+
+See L<Actium::Cmd::Time|Actium::Cmd::Time>.
+
+=head1 OPTIONS, CONFIGURATION AND ENVIRONMENT
+
+All commands in act-pvt.pl take the default option package. See
+L<Actium::Env::CLI|Actium::Env::CLI> for more information about the
+option packages. See information in each subcommand's module for
+specific information about that subcommand.
 
 =head1 DEPENDENCIES
 
@@ -107,22 +86,21 @@ Aaron Priven <apriven@actransit.org>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2017
+Copyright 2017-2018
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either:
 
 =over 4
 
-=item * the GNU General Public License as published by the Free
-Software Foundation; either version 1, or (at your option) any
-later version, or
+=item * the GNU General Public License as published by the Free Software
+Foundation; either version 1, or (at your option) any later version, or
 
 =item * the Artistic License version 2.0.
 
 =back
 
 This program is distributed in the hope that it will be useful, but
-WITHOUT  ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE.
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
