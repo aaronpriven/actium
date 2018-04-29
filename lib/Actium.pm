f14129bb1cb75e6c7d43660ad9b8296d30c483b9
Aaron Priven <aaron@priven.com>
2018-04-29 16:47:44 -0700

    Add S and SB to hardcoded no_locals list

--- lib/Actium.pm
+++ lib/Actium.pm
@@ -108,7 +108,7 @@ const my $CRLF              => qq{\cM\cJ};
 const my $SPACE             => q{ };
 const my $KEY_SEPARATOR     => "\c]";
 const my $MINS_IN_12HRS     => ( 12 * 60 );
-const my @TRANSBAY_NOLOCALS => (qw/FS L NX NX1 NX2 NX3 U W/);
+const my @TRANSBAY_NOLOCALS => (qw/FS L NX NX1 NX2 NX3 S SB U W/);
 
 const my @DIRCODES => qw( NB SB WB EB IN OU GO RT CW CC D1 D2 UP DN  A  B );
 #  Hastus                 0  1  3  2  4  5  6  7  8  9  10 11 12 13 14 15
@@ -550,7 +550,7 @@ sub joinseries {
         $separator .= $SPACE;
     }
     else {
-        $oxford = delete $options{oxford} // 0;
+        $oxford    = delete $options{oxford} // 0;
         $separator = q[, ];
     }
 
